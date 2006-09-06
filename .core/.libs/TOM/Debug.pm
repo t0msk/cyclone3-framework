38ed9eed383e882d5505007d6bea6d55bc1c2611
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-09-06 12:33:07 +0000

    [frame:libs] new: TOM::Debug::_obsolete_func() - pridana podpora pre volanie z bloku BEGIN {}

--- .core/.libs/TOM/Debug.pm
+++ .core/.libs/TOM/Debug.pm
@@ -12,22 +12,31 @@ use XML::Generator;
 
 package main;
 
+
+sub _obsolete {return 1;}
+
 sub _obsolete_func
 {
 	my $msg=shift;
 	my %env=@_;
 	
-	my ($package, $filename, $line, $subroutine) = caller(1);
-	my ($package2, $filename2, $line2) = caller(0);
+	my ($package3, $filename3, $line3, $subroutine3) = caller(3);
+	my ($package1, $filename1, $line1, $subroutine1) = caller(1);
+	my ($package, $filename, $line) = caller(0);
 	
-	main::_log("calling obsolete function '$subroutine' $msg",4);
+	if ($subroutine1=~/::BEGIN$/)
+	{
+		$filename1 = $filename3;
+	}
+	
+	main::_log("calling obsolete function '$subroutine1' $msg",4);
 	if ($msg)
 	{
-		main::_log("func:'$filename2:$line2:$subroutine' from:'$filename:$line' msg:{$msg}",4,"obsolete",1);
+		main::_log("func:'$filename:$line:$subroutine1' from:'$filename1:$line1' msg:{$msg}",4,"obsolete",1);
 	}
 	else
 	{
-		main::_log("func:'$filename2:$line2:$subroutine' from:'$filename:$line'",4,"obsolete",1);
+		main::_log("func:'$filename:$line:$subroutine1' from:'$filename1:$line1'",4,"obsolete",1);
 	}
 	
 	my $X = XML::Generator->new(':pretty');
@@ -36,11 +45,11 @@ sub _obsolete_func
 	$X->obsolete(
 		$X->type("function"),
 		$X->timestamp(time()),
-		$X->call_filename($filename),
-		$X->call_line($line),
-		$X->func_filename($filename2),
-		$X->func_line($line2),
-		$X->func($subroutine)
+		$X->call_filename($filename1),
+		$X->call_line($line1),
+		$X->func_filename($filename),
+		$X->func_line($line),
+		$X->func($subroutine1)
 	)."\n";
 	
 	open (HND_OBSOLETE,'>>'.$TOM::P.'/_logs/obsolete/'.$$.'.xml') || main::_log("obsolete $!");
