6517c7d569ff523422fb8f460ab2effc9acdbbda
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-09-29 17:56:23 +0000

    [frame] new: volne nastavenie unix group v ktorej je nainstalovany Cyclone3 a figuruju uzivatelia Na niektorych serveroch nemoze byt skupinou "cyclone3", pretoze skupina musi byt rovnaka ako defaultna skupina uzivatelov instalacie Cyclone3

--- .core/.libs/Cyclone/files.pm
+++ .core/.libs/Cyclone/files.pm
@@ -44,6 +44,18 @@ use Fcntl;
 
 =head1 VARIABLES
 
+=head2 $Cyclone::files::group
+
+Skupina do ktorej patri cyclone3 a vsetci uzivatelia. Pokial nieje $Cyclone::files::group nastavene v TOM.conf, potom sa pouzije defaultna hodnota "cyclone3".
+
+Pokial v danej instalacii pracuje viac uzivatelov (nielen uzivatel apache a cyclone3), potom je vhodne aby vsetci tito uzivatelia (okrem apache) mali default groupu rovnaku, inak dochadza ku konfliktom pri vytvarani suborov, na ktore ostatni ludia nemaju prava, prace so subversion, etc...
+
+Ina skupina sa nastavuje pomocou premennej $TOM::group v TOM.conf
+
+=cut
+
+our $group = $TOM::group || "cyclone3";
+
 =head2 @setid_d
 
 Zoznam regulárnych výrazov pre detekciu typu adresára + nastavenie práv
@@ -55,7 +67,7 @@ Zoznam regulárnych výrazov pre detekciu typu adresára + nastavenie práv
 our @setit_D=
 (
 	['media.*\.svn'                     ,"media .svn directory"        ,"770","www:www"],
-	['\.svn'                            ,".svn directory"              ,"570","apache:cyclone3"],
+	['\.svn'                            ,".svn directory"              ,"570","apache:$group"],
 	
 	# exclude dirs
 	['phprojekt'                       ,"phprojekt"                   ,"",""],
@@ -63,31 +75,33 @@ our @setit_D=
 	['^\.admin'                        ,"global admin"                ,"",""],
 	
 	# global
-	['^_data'                          ,"global _data"                ,"570","apache:cyclone3"],
-	['^_temp'                          ,"global _temp"                ,"770","apache:cyclone3"],
-	['^.symlinks$'                     ,"global .symlinks"            ,"570","apache:cyclone3"],
-	['^_mdl'                           ,"global _mdl"                 ,"570","apache:cyclone3"],
-	['^\.core'                         ,"global .core"                ,"570","apache:cyclone3"],
-	['^_dsgn$'                         ,"global _dsgn"                ,"570","apache:cyclone3"],
-	['^_type$'                         ,"global _type"                ,"570","apache:cyclone3"],
-	['^\.bin$'                         ,"global .bin"                 ,"770","cyclone3:cyclone3"],
-	['^_logs'                          ,"global _logs"                ,"770","apache:cyclone3"],
+	['^_data'                          ,"global _data"                ,"570","apache:$group"],
+	['^_temp'                          ,"global _temp"                ,"770","apache:$group"],
+	['^.symlinks$'                     ,"global .symlinks"            ,"570","apache:$group"],
+	['^_mdl'                           ,"global _mdl"                 ,"570","apache:$group"],
+	['^\.core'                         ,"global .core"                ,"570","apache:$group"],
+	['^_dsgn$'                         ,"global _dsgn"                ,"570","apache:$group"],
+	['^_type$'                         ,"global _type"                ,"570","apache:$group"],
+	['^\.bin$'                         ,"global .bin"                 ,"770","cyclone3:$group"],
+	['^_logs'                          ,"global _logs"                ,"770","apache:$group"],
 	['^!media'                         ,"global !media"               ,"775","www:www"],
-	['^_trash'                         ,"global _trash"               ,"777","cyclone3:cyclone3"],
+	['^_trash'                         ,"global _trash"               ,"777","cyclone3:$group"],
 	
 	# local
-	['\/_mdl$'                         ,"local _mdl"                  ,"570","apache:cyclone3"],
-	['\/\.libs'                        ,"local libraries"             ,"570","apache:cyclone3"],
- 	['\/_dsgn$'                        ,"local _dsgn"                 ,"770","apache:cyclone3"],
-	['\/!www$'                         ,"local !www"                  ,"770","apache:cyclone3"],
-	['\/_type$'                        ,"local _type"                 ,"770","apache:cyclone3"],
-	['\/_data'                         ,"local _data"                 ,"770","apache:cyclone3"],
-	['\/_logs'                         ,"local _logs"                 ,"770","apache:cyclone3"],
+	['\/_mdl$'                         ,"local _mdl"                  ,"570","apache:$group"],
+	['\/\.libs'                        ,"local libraries"             ,"570","apache:$group"],
+ 	['\/_dsgn$'                        ,"local _dsgn"                 ,"770","apache:$group"],
+	['\/!www$'                         ,"local !www"                  ,"770","apache:$group"],
+	['\/_type$'                        ,"local _type"                 ,"770","apache:$group"],
+	['\/_data'                         ,"local _data"                 ,"770","apache:$group"],
+	['\/_logs'                         ,"local _logs"                 ,"770","apache:$group"],
 	['\/!media'                        ,"local !media symlink"        ,"",""],
 	
-	['![\w\.\-]+$'                     ,"domain"                      ,"770","apache:cyclone3"],
-	['\/!www'                          ,"document roots"              ,"770","apache:cyclone3"],
-	['![\w\.\-]+/[\w]+$'               ,"subdomain"                   ,"770","apache:cyclone3"],
+	['![\w\.\-]+$'                     ,"domain"                      ,"770","apache:$group"],
+	['\/!www'                          ,"document roots"              ,"770","apache:$group"],
+	['![\w\.\-]+/[\w]+$'               ,"subdomain"                   ,"770","apache:$group"],
+	
+	['^.'                              ,"unknown"                     ,"","cyclone3:$group"],
 );
 
 
@@ -107,91 +121,92 @@ our @setit_F=
 	['!nc'                             ,"new Cyclone"                 ,"",""],
 	['phpmyadmin'                      ,"phpmyadmin"                  ,"",""],
 	
-	['_trash'                          ,"_trash"                      ,"660","apache:cyclone3"],
-	['^_temp/_Inline'                  ,"global Inline"               ,"777","apache:cyclone3"],
+	['_trash'                          ,"_trash"                      ,"660","apache:$group"],
+	['^_temp/_Inline'                  ,"global Inline"               ,"777","apache:$group"],
 	['^_temp'                          ,"global _temp"                ,"",""],
  
-	['\.(pwd)$'                        ,".pwd (password file)"        ,"660","apache:cyclone3"],
-	['\.tmpl$'                         ,".tmpl (template)"            ,"660","cyclone3:cyclone3"],
-	['\.sql$'                          ,".sql (SQL queries)"          ,"660","cyclone3:cyclone3"],
+	['\.(pwd)$'                        ,".pwd (password file)"        ,"660","apache:$group"],
+	['\.tmpl$'                         ,".tmpl (template)"            ,"660","cyclone3:$group"],
+	['\.sql$'                          ,".sql (SQL queries)"          ,"660","cyclone3:$group"],
 	
 	# www:www ak media su cez NFS na inom serveri
 	['^!media\/'                       ,"!media"                      ,"664","www:www"],
 	# apache:cyclone3 ak media su normalne lokalne
-	['!media\/'                        ,"!media domain"               ,"664","apache:cyclone3"],
-	['\.htaccess$'                     ,".htaccess"                   ,"460","apache:cyclone3"],
+	['!media\/'                        ,"!media domain"               ,"664","apache:$group"],
+	['\.htaccess$'                     ,".htaccess"                   ,"460","apache:$group"],
 	
 	['_logs\/.*\.log$'                 ,"_logs cron .log"             ,"",""],
 	['_logs\/httpd\/'                  ,"_logs httpd"                 ,"",""],
 	['_logs\/'                         ,"_logs cron .log"             ,"",""],
  
-	['\.libs\/.*\.pm'                  ,"library"              ,"460","apache:cyclone3"],
-	['\.libs\/.*\.txt'                 ,"library inputs"       ,"660","apache:cyclone3"],
+	['\.libs\/.*\.pm'                  ,"library"              ,"460","apache:$group"],
+	['\.libs\/.*\.txt'                 ,"library inputs"       ,"660","apache:$group"],
 	
 	
-	['\.bin\/'                         ,"core binary"                 ,"770","cyclone3:cyclone3"],
+	['\.bin\/'                         ,"core binary"                 ,"770","cyclone3:$group"],
 	
-	['^\.core\/_config\/httpd\.virtual\.conf',"HTTPD virtual conf"    ,"460","apache:cyclone3"],
-	['^\.core\/_config\/TOM.conf'      ,".core TOM.conf"              ,"460","apache:cyclone3"],
-	['^\.core\/_config\/'              ,".core conf"                  ,"460","apache:cyclone3"],
+	['^\.core\/_config\/httpd\.virtual\.conf',"HTTPD virtual conf"    ,"460","apache:$group"],
+	['^\.core\/_config\/TOM.conf'      ,".core TOM.conf"              ,"460","apache:$group"],
+	['^\.core\/_config\/'              ,".core conf"                  ,"460","apache:$group"],
 	
-	['^\.core\/_config\.sg\/httpd\.conf',"HTTPD virtual conf"         ,"460","apache:cyclone3"],
-	['^\.core\/_config\.sg\/TOM.conf'  ,".core TOM.conf"              ,"460","apache:cyclone3"],
+	['^\.core\/_config\.sg\/httpd\.conf',"HTTPD virtual conf"         ,"460","apache:$group"],
+	['^\.core\/_config\.sg\/TOM.conf'  ,".core TOM.conf"              ,"460","apache:$group"],
 	
 	
 	['^\.core\/.*pid$'                 ,".core .pid"                  ,"",""],
 	
 	['(^\.core\/(tom|cron|cyc|webclick|download|export|a540|test)|^core)'
-	                                   ,".core engines","770","cyclone3:cyclone3"],
+	                                   ,".core engines","770","cyclone3:$group"],
 	
-	['\.core\/.*\.so$'                 ,".core so"                    ,"660","apache:cyclone3"],
+	['\.core\/.*\.so$'                 ,".core so"                    ,"660","apache:$group"],
 	
-	['\.RFC$'                          ,"RFC files"                   ,"660","apache:cyclone3"],
-	['\/master\.conf$'                 ,"master conf"                 ,"660","apache:cyclone3"],
-	['\/local\.conf$'                  ,"local conf"                  ,"660","apache:cyclone3"],
-	['\/type\.conf$'                   ,"type conf"                   ,"660","apache:cyclone3"],
-	['\/rewrite\.conf$'                ,"rewrite conf"                ,"660","apache:cyclone3"],
-	['\/301\.conf$'                    ,"301 conf"                    ,"660","apache:cyclone3"],
+	['\.RFC$'                          ,"RFC files"                   ,"660","apache:$group"],
+	['\/master\.conf$'                 ,"master conf"                 ,"660","apache:$group"],
+	['\/local\.conf$'                  ,"local conf"                  ,"660","apache:$group"],
+	['\/type\.conf$'                   ,"type conf"                   ,"660","apache:$group"],
+	['\/rewrite\.conf$'                ,"rewrite conf"                ,"660","apache:$group"],
+	['\/301\.conf$'                    ,"301 conf"                    ,"660","apache:$group"],
  
-	['\/cron\..*?\.cml$'               ,"local cron cml"              ,"460","apache:cyclone3"],
+	['\/cron\..*?\.cml$'               ,"local cron cml"              ,"460","apache:$group"],
 	
 	
-	['!www.*(tom|pl|fcgi|php|asp)$'    ,"!www executables"            ,"560","apache:cyclone3"],
+	['!www.*(tom|pl|fcgi|php|asp)$'    ,"!www executables"            ,"560","apache:$group"],
 	['!www.*(css|html|wml|js|xml|txt|ppt|pdf|xls|xsl|doc|cvml)$'
-	                                   ,"!www docs"                   ,"660","apache:cyclone3"],
+	                                   ,"!www docs"                   ,"660","apache:$group"],
 	['!www.*php\?'
-	                                   ,"!www docs mirrored"          ,"660","apache:cyclone3"],
+	                                   ,"!www docs mirrored"          ,"660","apache:$group"],
 	['\/!www\/.*(jpg|png|gif|wbmp|swf|svg|ico)$'
-	                                   ,"!www graphix"                ,"660","apache:cyclone3"],
-	['\/!www\/.*(asf|avi)$'            ,"!www video"                  ,"660","apache:cyclone3"],
-	['\/!www\/.*(gz|bz2|tar|zip)$'     ,"!www archive"                ,"660","apache:cyclone3"],
-	['\/!www\/.*$'                     ,"!www unknown"                ,"660","apache:cyclone3"],
+	                                   ,"!www graphix"                ,"660","apache:$group"],
+	['\/!www\/.*(asf|avi)$'            ,"!www video"                  ,"660","apache:$group"],
+	['\/!www\/.*(gz|bz2|tar|zip)$'     ,"!www archive"                ,"660","apache:$group"],
+	['\/!www\/.*$'                     ,"!www unknown"                ,"660","apache:$group"],
 	
 	
-	['_dsgn\/.*\.dsgn$'                ,"_dsgn dsgn"                  ,"660","apache:cyclone3"],
-	['_type\/.*\.type$'                ,"_type type"                  ,"660","apache:cyclone3"],
-	['_type\/.*\.cml_type$'            ,"_type cml_type"              ,"660","apache:cyclone3"],
-	['_type\/.*\.cml_gen$'             ,"_type cml_gen"               ,"660","apache:cyclone3"],
+	['_dsgn\/.*\.dsgn$'                ,"_dsgn dsgn"                  ,"660","apache:$group"],
+	['_type\/.*\.type$'                ,"_type type"                  ,"660","apache:$group"],
+	['_type\/.*\.cml_type$'            ,"_type cml_type"              ,"660","apache:$group"],
+	['_type\/.*\.cml_gen$'             ,"_type cml_gen"               ,"660","apache:$group"],
 	
-	['_mdl\/.*\.xlng$'                 ,"xlng"                        ,"660","apache:cyclone3"],
-	['_mdl.*\.xsgn$'                   ,"xsgn"                        ,"660","apache:cyclone3"],
-	['_mdl\/.*mdl$'                    ,"?mdl"                        ,"660","apache:cyclone3"],
+	['_mdl\/.*\.xlng$'                 ,"xlng"                        ,"660","apache:$group"],
+	['_mdl.*\.xsgn$'                   ,"xsgn"                        ,"660","apache:$group"],
+	['_mdl\/.*mdl$'                    ,"?mdl"                        ,"660","apache:$group"],
 	
-	['_mdl\/.*cron$'                   ,"_mdl cron"                   ,"660","apache:cyclone3"],
+	['_mdl\/.*cron$'                   ,"_mdl cron"                   ,"660","apache:$group"],
  
 	['_data\/USRM\/.*'                 ,"local _data USRM"            ,"",""],
-	['_data\/.*'                       ,"local _data"                 ,"660","apache:cyclone3"],
+	['_data\/.*'                       ,"local _data"                 ,"660","apache:$group"],
 	
 	# old admin
-	['!admin/.*\.mdl$'                 ,"local !admin *.mdl","460","apache:cyclone3"],
-	['!admin/.*\.pl$'                  ,"local !admin *.pl","570","apache:cyclone3"],
-	['!admin'                          ,"local !admin","460","apache:cyclone3"],
+	['!admin/.*\.mdl$'                 ,"local !admin *.mdl","460","apache:$group"],
+	['!admin/.*\.pl$'                  ,"local !admin *.pl","570","apache:$group"],
+	['!admin'                          ,"local !admin","460","apache:$group"],
 	
-	['^version\.[0-9]+$'               ,"version"                     ,"660","apache:cyclone3"],
-	['^version$'                       ,"version"                     ,"660","apache:cyclone3"],
+	['^version\.[0-9]+$'               ,"version"                     ,"660","apache:$group"],
+	['^version$'                       ,"version"                     ,"660","apache:$group"],
 	
-	['\.key$'                          ,"key file"                    ,"660","apache:cyclone3"],
- 
+	['\.key$'                          ,"key file"                    ,"660","apache:$group"],
+	
+	['^.'                              ,"unknown"                     ,"","cyclone3:$group"],
 );
 
 
