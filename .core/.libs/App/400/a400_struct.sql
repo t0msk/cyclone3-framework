e10899f42104edbb9d96310312513f1ec0c2c860
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-10-11 18:06:27 +0000

    [*][frame:a400] a400_struct.sql - upgrade datovej struktury na MySQL 4.1

--- .core/.libs/App/400/a400_struct.sql
+++ .core/.libs/App/400/a400_struct.sql
@@ -1,32 +1,32 @@
 -- app=a400
+-- version=4.1
 
 -- --------------------------------------------------------
 
 CREATE TABLE `/*db_name*/`.`/*app*/` (
   `ID` int(10) unsigned NOT NULL auto_increment,
-  `IDname` varchar(255) default NULL,
+  `IDname` varchar(255) character set ascii collate ascii_bin default NULL,
   `IDattrs` int(10) unsigned default NULL,
-  `IDcategory` varchar(32) binary NOT NULL default '',
-  `priority` varchar(17) binary NOT NULL default '00000000000000000',
+  `IDcategory` varchar(32) character set ascii collate ascii_bin NOT NULL default '',
+  `priority` varchar(17) character set ascii NOT NULL default '00000000000000000',
   `starttime` int(10) unsigned NOT NULL default '0',
   `endtime` int(10) unsigned NOT NULL default '0',
   `changetime` int(10) unsigned NOT NULL default '0',
   `lasttime` int(10) unsigned NOT NULL default '0',
   `IDauthor` smallint(5) unsigned NOT NULL default '0',
   `IDeditor` smallint(5) unsigned NOT NULL default '0',
-  `title` varchar(100) NOT NULL default '',
-  `subtitle` varchar(150) NOT NULL default '',
-  `tiny` text NOT NULL,
-  `full` text NOT NULL,
+  `title` varchar(100) character set utf8 collate utf8_bin NOT NULL default '',
+  `subtitle` varchar(150) character set utf8 collate utf8_bin NOT NULL default '',
+  `tiny` text character set utf8 collate utf8_bin NOT NULL,
+  `full` text character set utf8 collate utf8_bin NOT NULL,
   `visits` mediumint(8) unsigned NOT NULL default '0',
   `link` int(10) unsigned NOT NULL default '0', -- linkovanie sa v praxi už nepoužíva
-  `xrelated` text NOT NULL,
-  `xdata` text NOT NULL, -- starší ekvivalent pre cvml_data
---  `private` char(1) NOT NULL default 'N', -- určuje či je daný článok verejný alebo patrí do privátnej sekcie
-  `lng` varchar(2) NOT NULL default '',
-  `active` char(1) NOT NULL default 'N',
-  `arch` char(1) NOT NULL default 'N',
-  PRIMARY KEY  (`ID`,`starttime`,`active`,`lng`,`arch`), -- toto je riadok ktory zabezpecuje unikatnost
+  `xrelated` text character set utf8 collate utf8_bin NOT NULL,
+  `xdata` text character set utf8 collate utf8_bin NOT NULL,
+  `lng` char(2) character set ascii NOT NULL default '',
+  `active` char(1) character set ascii NOT NULL default 'N',
+  `arch` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`starttime`,`active`,`lng`,`arch`),
   KEY `starttime` (`starttime`),
   KEY `priority` (`priority`),
   KEY `IDattrs` (`IDattrs`),
@@ -35,45 +35,43 @@ CREATE TABLE `/*db_name*/`.`/*app*/` (
   KEY `IDname` (`IDname`),
   FULLTEXT KEY `MATSEARCH` (`full`,`tiny`,`subtitle`,`title`),
   FULLTEXT KEY `xrelated` (`xrelated`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 CREATE TABLE `/*db_name*/`.`/*app*/_arch` (
-  `ID` int(10) unsigned NOT NULL default '0',
-  `IDname` varchar(255) default NULL,
+  `ID` int(10) unsigned NOT NULL auto_increment,
+  `IDname` varchar(255) character set ascii collate ascii_bin default NULL,
   `IDattrs` int(10) unsigned default NULL,
-  `IDcategory` varchar(32) binary NOT NULL default '',
-  `priority` varchar(17) binary NOT NULL default '00000000000000000',
+  `IDcategory` varchar(32) character set ascii collate ascii_bin NOT NULL default '',
+  `priority` varchar(17) character set ascii NOT NULL default '00000000000000000',
   `starttime` int(10) unsigned NOT NULL default '0',
   `endtime` int(10) unsigned NOT NULL default '0',
   `changetime` int(10) unsigned NOT NULL default '0',
   `lasttime` int(10) unsigned NOT NULL default '0',
   `IDauthor` smallint(5) unsigned NOT NULL default '0',
   `IDeditor` smallint(5) unsigned NOT NULL default '0',
-  `title` varchar(100) NOT NULL default '',
-  `subtitle` varchar(150) NOT NULL default '',
-  `tiny` tinytext NOT NULL,
-  `full` text NOT NULL,
+  `title` varchar(100) character set utf8 collate utf8_bin NOT NULL default '',
+  `subtitle` varchar(150) character set utf8 collate utf8_bin NOT NULL default '',
+  `tiny` text character set utf8 collate utf8_bin NOT NULL,
+  `full` text character set utf8 collate utf8_bin NOT NULL,
   `visits` mediumint(8) unsigned NOT NULL default '0',
-  `link` int(10) unsigned NOT NULL default '0',
-  `xrelated` text NOT NULL,
-  `xdata` text NOT NULL,
---  `private` char(1) NOT NULL default 'N',
-  `lng` varchar(2) NOT NULL default '',
-  `active` char(1) NOT NULL default 'N',
-  `arch` char(1) NOT NULL default 'Y',
-  PRIMARY KEY  (`ID`,`starttime`,`arch`,`active`,`lng`),
+  `link` int(10) unsigned NOT NULL default '0', -- linkovanie sa v praxi už nepoužíva
+  `xrelated` text character set utf8 collate utf8_bin NOT NULL,
+  `xdata` text character set utf8 collate utf8_bin NOT NULL,
+  `lng` char(2) character set ascii NOT NULL default '',
+  `active` char(1) character set ascii NOT NULL default 'N',
+  `arch` char(1) character set ascii NOT NULL default 'Y',
+  PRIMARY KEY  (`ID`,`starttime`,`active`,`lng`,`arch`),
   KEY `starttime` (`starttime`),
-  KEY `IDcategory` (`IDcategory`),
+  KEY `priority` (`priority`),
   KEY `IDattrs` (`IDattrs`),
+  KEY `IDcategory` (`IDcategory`),
   KEY `endtime` (`endtime`),
-  KEY `lng` (`lng`),
-  KEY `active` (`active`),
   KEY `IDname` (`IDname`),
   FULLTEXT KEY `MATSEARCH` (`full`,`tiny`,`subtitle`,`title`),
   FULLTEXT KEY `xrelated` (`xrelated`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -84,7 +82,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/_attrs` (
   `votes_points` int(11) NOT NULL default '0',
   PRIMARY KEY  (`IDattrs`),
   KEY `visits_index7day` (`visits_index7day`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -94,19 +92,19 @@ CREATE TABLE `/*db_name*/`.`/*app*/_attrs_arch` (
   `votes_count` int(10) unsigned NOT NULL default '0',
   `votes_points` int(11) NOT NULL default '0',
   PRIMARY KEY  (`IDattrs`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 CREATE TABLE `/*db_name*/`.`/*app*/_category` (
-  `ID` varchar(32) binary NOT NULL default '',
-  `IDname` varchar(255) default NULL,
-  `name` varchar(100) binary NOT NULL default '',
-  `xrelated` text NOT NULL,
-  `xdata` text NOT NULL,
-  `lng` varchar(2) NOT NULL default '',
-  `active` char(1) NOT NULL default 'N',
+  `ID` varchar(32) character set ascii collate ascii_bin NOT NULL default '',
+  `IDname` varchar(255) character set ascii collate ascii_bin default NULL,
+  `name` varchar(100) character set utf8 collate utf8_bin NOT NULL default '',
+  `xrelated` text character set utf8 collate utf8_bin NOT NULL,
+  `xdata` text character set utf8 collate utf8_bin NOT NULL,
+  `lng` char(2) character set ascii NOT NULL default '',
+  `active` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`lng`),
   KEY `name` (`name`),
   KEY `IDname` (`IDname`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
