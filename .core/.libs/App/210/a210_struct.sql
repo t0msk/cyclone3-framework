a469aa8eba3ef0c5ad79d84e1e03576ea39cabc5
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-10-18 13:01:41 +0000

    [frame:a210] a210_struct.sql - zmena nazvu tabuliek, status je default N

--- .core/.libs/App/210/a210_struct.sql
+++ .core/.libs/App/210/a210_struct.sql
@@ -4,14 +4,14 @@
 
 -- --------------------------------------------------
 
-CREATE TABLE `/*db_name*/`.`/*app*/_page_cat` (
+CREATE TABLE `/*db_name*/`.`/*app*/_page` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL,
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
   `name` varchar(32) character set utf8 collate utf8_bin NOT NULL default '',
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
-  `status` char(1) character set ascii NOT NULL default 'Y',
+  `status` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`datetime_create`),
   UNIQUE KEY `UNI_0` (`ID_entity`,`lng`),
   UNIQUE KEY `UNI_1` (`ID_charindex`,`lng`)
@@ -19,13 +19,13 @@ CREATE TABLE `/*db_name*/`.`/*app*/_page_cat` (
 
 -- --------------------------------------------------
 
-CREATE TABLE `/*db_name*/`.`/*app*/_page_cat_j` (
+CREATE TABLE `/*db_name*/`.`/*app*/_page_j` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL,
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
   `name` varchar(32) character set utf8 collate utf8_bin NOT NULL default '',
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
-  `status` char(1) character set ascii NOT NULL default 'Y',
+  `status` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`datetime_create`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
