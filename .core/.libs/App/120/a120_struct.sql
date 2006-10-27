a4b2bbe148fc0dd1e864137765989c7b050067d3
mgregor <mgregor@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-10-27 12:28:16 +0000

    [+][frame:a120] a120_struct.sql - pridane stlpce email a homepage do tabulky a120 a upravene create statementy tabuliek a120 a a120_category, aby vyhovovali specifikacii

--- .core/.libs/App/120/a120_struct.sql
+++ .core/.libs/App/120/a120_struct.sql
@@ -1,5 +1,6 @@
 
 -- app=a120
+-- version=4.1
 
 -- --------------------------------------------------
 
@@ -7,18 +8,22 @@ CREATE TABLE `/*db_name*/`.`/*app*/` (
   `ID` smallint(5) unsigned NOT NULL auto_increment,
   `IDcategory` smallint(5) unsigned NOT NULL default '0',
   `IDtype` tinyint(4) unsigned NOT NULL default '0',
-  `nickname` varchar(50) NOT NULL default '',
-  `fullname` varchar(250) binary NOT NULL default '',
-  `xdata` text NOT NULL,
-  `active` char(1) NOT NULL default 'N',
+  `nickname` varchar(50) character set ascii NOT NULL default '',
+  `fullname` varchar(250) character set utf8 collate utf8_bin NOT NULL default '',
+  `email` varchar(60) character set ascii NOT NULL default '',
+  `homepage` varchar(250) character set ascii NOT NULL default '',
+  `xdata` text character set utf8 collate utf8_bin NOT NULL,
+  `active` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`)
-) TYPE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
 
 CREATE TABLE `/*db_name*/`.`/*app*/_category` (
   `ID` smallint(5) unsigned NOT NULL auto_increment,
   `IDre` smallint(5) unsigned NOT NULL default '0',
-  `name` varchar(100) binary NOT NULL default '',
+  `name` varchar(100) character set utf8 collate utf8_bin NOT NULL default '',
   PRIMARY KEY  (`ID`)
-) TYPE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------
