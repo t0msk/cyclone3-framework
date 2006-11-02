9c7706325b3a997d41db1784c8f1d4cbe9689400
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-11-02 16:05:05 +0000

    [!][frame:a120] a120_struct.sql - oprava nekompatibilnych collumnov 4.0<->4.1

--- .core/.libs/App/120/a120_struct.sql
+++ .core/.libs/App/120/a120_struct.sql
@@ -9,10 +9,10 @@ CREATE TABLE `/*db_name*/`.`/*app*/` (
   `IDcategory` smallint(5) unsigned NOT NULL default '0',
   `IDtype` tinyint(4) unsigned NOT NULL default '0',
   `nickname` varchar(50) character set ascii NOT NULL default '',
-  `fullname` varchar(250) character set utf8 collate utf8_bin NOT NULL default '',
+  `fullname` varchar(120) character set utf8 collate utf8_bin NOT NULL default '',
   `email` varchar(60) character set ascii NOT NULL default '',
-  `homepage` varchar(250) character set ascii NOT NULL default '',
-  `xdata` text character set utf8 collate utf8_bin NOT NULL,
+  `homepage` varchar(200) character set ascii NOT NULL default '',
+  `xdata` text character set utf8 NOT NULL,
   `active` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
