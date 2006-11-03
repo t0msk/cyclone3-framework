3123c62c35719e2bbf7506f4bb744070cb4342fe
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-11-03 07:47:00 +0000

    [+][frame:libs] TOM::Database::SQL::transaction - vytvorena podpora pre transakcie - v sucasnosti len InnoDB tabulky my $tr=new TOM::Database::SQL::transaction('db_h'=>"main"); # SQL... $tr->close();

---
+++ .core/.libs/TOM/Database/SQL/transaction.pm
@@ -0,0 +1,131 @@
+package TOM::Database::SQL::transaction;
+
+=head1 NAME
+
+TOM::Database::SQL
+
+=cut
+
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+
+our %handler;
+
+=head1 FUNCTIONS
+
+=head2 new()
+
+Začiatok transakcie
+
+=cut
+
+sub new
+{
+	my $class=shift;
+	my $self={};
+	
+	my %env=@_;
+	
+	$env{'db_h'}="main" unless $env{'db_h'};
+	
+	$self->{'db_h'}=$env{'db_h'};
+	
+	if ($handler{$self->{'db_h'}})
+	{
+		main::_log("<={SQL:$self->{'db_h'}} TRANSACTION STARTED IN TRANSACTION ($handler{$self->{'db_h'}}+)");
+	}
+	else
+	{
+		
+		main::_log("<={SQL:$self->{'db_h'}} START TRANSACTION");
+		
+		my $SQL="SET AUTOCOMMIT=0";
+		my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'log'=>1);
+		
+		my $SQL="START TRANSACTION WITH CONSISTENT SNAPSHOT";
+		my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'log'=>1);
+		
+	}
+	
+	$handler{$self->{'db_h'}}++;
+	
+	return bless $self, $class;
+}
+
+
+sub close
+{
+	my $self=shift;
+	
+	if (!$handler{$self->{'db_h'}})
+	{
+		main::_log("<={SQL:$self->{'db_h'}} CANCEL ENDING TRANSACTION");
+		delete $self->{'db_h'};
+		return undef;
+	}
+	
+	$handler{$self->{'db_h'}}--;
+	
+	if (!$handler{$self->{'db_h'}})
+	{
+		main::_log("<={SQL:$self->{'db_h'}} END TRANSACTION");
+		my $SQL="SET AUTOCOMMIT=1";
+		my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'log'=>1);
+	}
+	else
+	{
+		main::_log("<={SQL:$self->{'db_h'}} CAN'T END TRANSACTION ($handler{$self->{'db_h'}}-)");
+	}
+	
+	delete $self->{'db_h'};
+	return undef;
+}
+
+
+sub rollback
+{
+	my $self=shift;
+	main::_log("<={SQL:$self->{'db_h'}} ROLLBACK");
+	
+	undef $handler{$self->{'db_h'}};
+	
+	my $SQL="ROLLBACK";
+	my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'log'=>1);
+	
+	my $SQL="SET AUTOCOMMIT=1";
+	my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'log'=>1);
+	
+	delete $self->{'db_h'};
+	return undef;
+}
+
+
+sub DESTROY
+{
+	my $self=shift;
+	return undef unless $self->{'db_h'}; # return ak uz bol object zniceny
+	return undef unless $handler{$self->{'db_h'}}; # return ak uz bol object zniceny vnutornou transakciou
+	
+	undef $handler{$self->{'db_h'}};
+	
+	main::_log("<={SQL:$self->{'db_h'}} DESTROY TRANSACTION",1);
+	
+	my $SQL="ROLLBACK";
+	my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'quiet'=>1);
+	
+	my $SQL="SET AUTOCOMMIT=1";
+	my @eout=TOM::Database::SQL::execute($SQL,'db_h'=>$self->{'db_h'},'quiet'=>1);
+	
+ 	die "Not ended transaction on handler '$self->{'db_h'}'.";
+	
+	$self={};
+	
+	return undef;
+}
+
+
+1;
