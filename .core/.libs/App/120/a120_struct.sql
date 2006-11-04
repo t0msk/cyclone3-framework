63398c1daf0b8b56b48ba4b012abb92ba5b753fb
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-11-04 15:52:50 +0000

    [*][frame:a120] a120_struct.sql - uprava collumns pre vyhladavanie a sortovanie (minor)

--- .core/.libs/App/120/a120_struct.sql
+++ .core/.libs/App/120/a120_struct.sql
@@ -8,11 +8,11 @@ CREATE TABLE `/*db_name*/`.`/*app*/` (
   `ID` smallint(5) unsigned NOT NULL auto_increment,
   `IDcategory` smallint(5) unsigned NOT NULL default '0',
   `IDtype` tinyint(4) unsigned NOT NULL default '0',
-  `nickname` varchar(50) character set ascii NOT NULL default '',
-  `fullname` varchar(120) character set utf8 collate utf8_bin NOT NULL default '',
+  `nickname` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `fullname` varchar(120) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `email` varchar(60) character set ascii NOT NULL default '',
   `homepage` varchar(200) character set ascii NOT NULL default '',
-  `xdata` text character set utf8 NOT NULL,
+  `xdata` text NOT NULL,
   `active` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -22,7 +22,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/` (
 CREATE TABLE `/*db_name*/`.`/*app*/_category` (
   `ID` smallint(5) unsigned NOT NULL auto_increment,
   `IDre` smallint(5) unsigned NOT NULL default '0',
-  `name` varchar(100) character set utf8 collate utf8_bin NOT NULL default '',
+  `name` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
