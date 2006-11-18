044b607682449e16b2f40103398c39e2f33f5e21
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-11-18 12:17:45 +0000

    [!][frame:pub] safe SIG v perli nedovolili obsluhu v XS rutinach - prepisanie SIG pomocou POSIX

--- .core/.libs/TOM/Engine/pub.pm
+++ .core/.libs/TOM/Engine/pub.pm
@@ -32,6 +32,7 @@ BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__.'{$Id$}');};}
 	
 	
 	# CORE Engine kniznice
+	use TOM::Engine::pub::SIG;
 	use TOM::Debug::pub;
 	use TOM::Warning;
 	use TOM::Database::connect;
---
+++ .core/.libs/TOM/Engine/pub/SIG.pm
@@ -0,0 +1,63 @@
+package TOM::Engine::pub::SIG;
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+
+sub handler_exit
+{
+	my $signame = shift;
+	print "Location: http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}\n\n";
+	main::_log("SIG '$signame' (relocation) (timeout $TOM::fcgi_timeout secs, lives ".(time()-$TOM::time_start)." secs, $tom::count requests) PID:$$ domain:$tom::H",3,"pub.mng",1);
+	exit(0);
+}
+
+sub handler_wait
+{
+	my $signame=shift;
+	main::_log("REQUEST SIG '$signame' (lives ".
+			(time()-$TOM::time_start).
+			" secs, $tom::count requests) PID:$$ domain:$tom::H ($@ $!)",3,"pub.mng",1);
+	if ($main::sig_term)
+	{
+		print "Location: http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}\n\n";
+		main::_log("ACCEPTING SIG '$signame'",3,"pub.mng",1);
+		exit(0);
+	}
+	else
+	{
+		$tom::HUP=2; # po dobehnuti tohto requestu na 100% exitnem
+	};
+}
+
+
+our $sigset = POSIX::SigSet->new();
+
+
+our $action_exit = POSIX::SigAction->new(
+	\&TOM::Engine::pub::SIG::handler_exit,
+	$sigset,
+	&POSIX::SA_NODEFER);
+
+
+our $action_wait = POSIX::SigAction->new(
+	\&TOM::Engine::pub::SIG::handler_wait,
+	$sigset,
+	&POSIX::SA_NODEFER);
+	
+	
+main::_log("registering SIG{ALRM} action to exit");
+POSIX::sigaction(&POSIX::SIGALRM, $action_exit);
+
+main::_log("registering SIG{HUP} action to exit");
+POSIX::sigaction(&POSIX::SIGHUP, $action_exit);
+
+main::_log("registering SIG{TERM} action to wait");
+POSIX::sigaction(&POSIX::SIGTERM, $action_wait);
+
+
+
+1;
