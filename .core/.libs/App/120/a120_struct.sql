c5b1ac4377bc9a8f47c74c8073e9759fe745aae2
mgregor <mgregor@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2006-12-12 14:28:18 +0000

    [+][frame:a120] doplnenie pola is_default a modulov a type pre jeho editaciu

--- .core/.libs/App/120/a120_struct.sql
+++ .core/.libs/App/120/a120_struct.sql
@@ -1,6 +1,6 @@
 
 -- app=a120
--- version=4.1
+-- version=5.0
 
 -- --------------------------------------------------
 
@@ -12,6 +12,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/` (
   `fullname` varchar(120) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `email` varchar(60) character set ascii NOT NULL default '',
   `homepage` varchar(200) character set ascii NOT NULL default '',
+  `is_default` char(1) character set ascii NOT NULL default 'N',
   `xdata` text NOT NULL,
   `active` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`)
