0127cfd24fefb129a2005aa2d65ca14f5cc527a2
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-01-04 10:22:53 +0000

    [*][frame:*] presunutie pod2docbook funkcionality do addonu pod2docbook

---
+++ _addons/Ext/pod2docbook/_init.pm
@@ -0,0 +1,67 @@
+#!/bin/perl
+package Ext::pod2docbook;
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+=head1 NAME
+
+Extension pod2docbook
+
+=head1 DESCRIPTION
+
+Library that generates DocBook files from POD documentation
+
+=cut
+
+BEGIN
+{
+	main::_log("<={LIB} ".__PACKAGE__);
+	my $dir=(__FILE__=~/^(.*)\//)[0].'/src';
+	unshift @INC, $dir;
+}
+
+BEGIN {require Pod::DocBook;}
+
+BEGIN {shift @INC;}
+
+
+use TOM::Temp::file;
+
+
+sub pod2docbook
+{
+	my $data=shift;
+	my %env=shift;
+	
+	my $t=track TOM::Debug(__PACKAGE__."::pod2docbook");
+	
+	my $parser = Pod::DocBook->new
+	(
+		doctype => 'article',
+		fix_double_quotes => 1,
+		spaces => 3
+	);
+	
+	my $tmp_pod=TOM::Temp::file->new('ext'=>'pm');
+	$tmp_pod->save_content($data);
+	my $tmp_docbook=TOM::Temp::file->new('ext'=>'docbook');
+	
+	$parser->parse_from_file($tmp_pod->{'filename'}, $tmp_docbook->{'filename'});
+	
+	open(HND,'<'.$tmp_docbook->{'filename'});local $/;
+	my $output=<HND>;
+	
+	$t->close();
+	return $output;
+}
+
+
+1;
+
+=head1 AUTHOR
+
+Roman Fordinal
+
+=cut
