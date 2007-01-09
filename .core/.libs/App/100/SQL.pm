9c3004ac83037f5c87f141b767e5766de4cbfce5
mgregor <mgregor@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-01-09 09:13:52 +0000

    [+][frame:a100] upravy journalovania a nova funkcia pre uzavretie ticketu

--- .core/.libs/App/100/SQL.pm
+++ .core/.libs/App/100/SQL.pm
@@ -23,18 +23,22 @@ use App::100;
 
 =head1 FUNCTIONS
 
-=head2 ticket_new
+=head2 ticket_event_new
 
 Vloží nový záznam
 
+=head2 ticket_close
+
+Uzavrie ticket
+
 =cut
 
-our $debug=1;
+our $debug=0;
 
 sub ticket_event_new
 {
 	my %env=@_;
-	my $t=track TOM::Debug(__PACKAGE__."::page_set_as_default()");
+	my $t=track TOM::Debug(__PACKAGE__."::ticket_event_new()");
 	
 	$env{'db_h'}='main' unless $env{'db_h'};
 	$env{'db_name'}=$TOM::DB{$env{'db_h'}}{'name'} unless $env{'db_name'};
@@ -49,7 +53,7 @@ sub ticket_event_new
 	# Ak existuje tiket z danym nazvom, tak
 	my $sql = qq/
 	SELECT
-		ID
+		ID, emails, status
 	FROM
 		TOM.a100_ticket
 	WHERE
@@ -78,6 +82,8 @@ sub ticket_event_new
 	{
 		my %ticket = $sth0{'sth'}->fetchhash;
 		$ID_ticket = $ticket{'ID'};
+		my $journalize;
+		$journalize=1 if $ticket{'status'} ne 'Y';
 		App::020::SQL::functions::update(
 			'db_h' => "main",
 			'db_name' => "TOM",
@@ -86,9 +92,10 @@ sub ticket_event_new
 			'columns' =>
 			{
 				'status' => "'Y'",
-			}
+				'emails' => "'$env{'emails'}'",
+			},
+			'-journalize' => $journalize
 		);
-		
 	}
 
 	return 0 unless $ID_ticket;
@@ -108,6 +115,43 @@ sub ticket_event_new
 	);
 	
 	return 0 unless $ID_ticket_event;
+	$t->close();
+	return 1;
+}
+
+sub ticket_close
+{
+	my %env=@_;
+	my $t=track TOM::Debug(__PACKAGE__."::ticket_close()");
+	
+	$env{'db_h'}='main' unless $env{'db_h'};
+	$env{'db_name'}=$TOM::DB{$env{'db_h'}}{'name'} unless $env{'db_name'};
+
+	return 0 unless $env{'ID'};
+
+	App::020::SQL::functions::update(
+		'db_h' => 'main',
+		'db_name' => 'TOM',
+		'tb_name' => 'a100_ticket',
+		'ID' => $env{'ID'},
+		'columns' =>
+		{
+			'status' => "'N'",
+		},
+		'-journalize' => 1
+	);
+
+	App::020::SQL::functions::update(
+		'db_h' => 'main',
+		'db_name' => 'TOM',
+		'tb_name' => 'a100_ticket_event',
+		'ID_ticket' => $env{'ID'},
+		'columns' =>
+		{
+			'status' => "'N'",
+		}
+	);
+
 	$t->close();
 	return 1;
 }
