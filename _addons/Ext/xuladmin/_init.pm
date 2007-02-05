ce0af67ca23aa051c406cc6e32b251e0e40e91f4
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-02-05 19:53:17 +0000

    [+][frame:xuladmin] added Extension 'xuladmin' with proof-of-concept documentation file

---
+++ _addons/Ext/xuladmin/_init.pm
@@ -0,0 +1,31 @@
+#!/bin/perl
+package Ext::xuladmin;
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+=head1 NAME
+
+Extension xuladmin
+
+=head1 DESCRIPTION
+
+This extension gives support and stores datas to XUL Cyclone3 administration frontend named shortly 'xuladmin'
+
+=cut
+
+BEGIN
+{
+	main::_log("<={LIB} ".__PACKAGE__);
+}
+
+our $DIR=(__FILE__=~/^(.*)\//)[0];
+
+1;
+
+=head1 AUTHOR
+
+Roman Fordinal (roman.fordinal@comsultia.com)
+
+=cut
