0abbe1342d6c5ff4779ca007d6b2e2095061787b
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-02-06 15:24:17 +0000

    [+][frame:docbook4CMS] added translation of images

--- _addons/Ext/docbook4CMS/_init.pm
+++ _addons/Ext/docbook4CMS/_init.pm
@@ -21,6 +21,7 @@ BEGIN
 }
 
 use XML::Sablotron;
+use File::Copy;
 
 our $DIR=(__FILE__=~/^(.*)\//)[0];
 
@@ -31,6 +32,13 @@ our $DIR=(__FILE__=~/^(.*)\//)[0];
 =head2 docbook2xhtml('<?xml>')
 
 Convert DocBook XML content into XHTML content
+
+  'translate_images' =>
+  {
+  	'dir_from' => '/www/TOM/_temp/' ?
+  	'dir_to' => '/www/TOM/!example.tld/!media/grf/temp'
+  	'uri' => 'http://media.example.tld/grf/temp'
+  }
 
 =cut
 
@@ -52,10 +60,11 @@ sub sab_MHError
 	die "Dying from Sablotron errors, see log\n";
 }
 
+
 sub docbook2xhtml
 {
 	my $data=shift;
-	my %env=shift;
+	my %env=@_;
 	
 	my $t=track TOM::Debug(__PACKAGE__."::docbook2xhtml()");
 	
@@ -75,8 +84,51 @@ sub docbook2xhtml
 	my $output=$sab->getResultArg('arg:/output');
 	$output=~s|^<\?xml.*?>||;
 	main::_log("output=length(".(length($output)).")");
+	
+	# postprocessing
+	if ($env{'translate_images'})
+	{
+		$output=~s/<img(.*?)src="(.*?)"/'<img'.$1.'src="'.(
+			_docbook2xhtml_translate_image(
+				$2,
+				$env{'translate_images'}{'dir_from'},
+				$env{'translate_images'}{'dir_to'},
+				$env{'translate_images'}{'uri'}
+			)
+		).'"'/eg;
+	}
+	
 	$t->close();
 	return $output;
 }
+
+
+sub _docbook2xhtml_translate_image
+{
+	my $link=shift;
+	my $dir_from=shift;
+	my $dir_to=shift;
+	my $uri=shift;
+	main::_log("link='$link'");
+	
+	my $hash=Utils::vars::genhash(8);
+	
+	my $from=$TOM::P.'/_addons/Ext/xuladmin/help/'.$link;
+	my $hash=Digest::MD5::md5_base64($from);
+	my $to=$dir_to.'/'.$hash.'-'.$link;
+	my $uri_to=$uri.'/'.$hash.'-'.$link;
+	
+	main::_log("from='$from' to='$to'");
+	
+	if (-e $to)
+	{
+		return $uri_to;
+	}
+	
+	File::Copy::copy($from,$to);
+	
+	return $uri_to;
+}
+
 
 1;
