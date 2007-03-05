ae036ab392a4b1b774a2630b84b6a5f23c262329
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-03-05 10:43:02 +0000

    [*][frame:a400] a400 moved into _addons

--- .core/.libs/App/400.pm
+++ .core/.libs/App/400.pm
@@ -1,12 +1,11 @@
 #!/bin/perl
 package App::400;
-use App::400::SQL; # pytam si SQL a SQL si pyta vsetko pod nim
-use strict;
 
-BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+BEGIN
+{
+	main::_obsolete_func();
+}
 
-our @ISA=("App::400::SQL"); # import SQL prototypov
-
-
+use App::400::_init;
 
 1;
