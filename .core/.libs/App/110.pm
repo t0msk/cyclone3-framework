54f6b865ab59ead5396eb24033086e25580977db
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-03-05 10:50:30 +0000

    [*][frame:a110] a110 moved into _addons

--- .core/.libs/App/110.pm
+++ .core/.libs/App/110.pm
@@ -1,20 +1,11 @@
 #!/bin/perl
 package App::110;
-use open ':utf8', ':std';
-use encoding 'utf8';
-use utf8;
-use strict;
 
-BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+BEGIN
+{
+	main::_obsolete_func();
+}
 
-
-
-
-
-
-
-
-
-
+use App::110::_init;
 
 1;
---
+++ _addons/App/110/_init.pm
@@ -0,0 +1,20 @@
+#!/bin/perl
+package App::110;
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+
+
+
+
+
+
+
+
+
+
+1;
