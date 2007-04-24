e57d32ff09768bfba522e3174212d2eb501c60b9
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-04-24 19:34:55 +0000

    [*][frame:libs] TOM::Engine::pub::SIG - handling SIGPIPE, stop SIGALRM

--- .core/.libs/TOM/Engine/pub/SIG.pm
+++ .core/.libs/TOM/Engine/pub/SIG.pm
@@ -10,7 +10,10 @@ BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
 sub handler_exit
 {
 	my $signame = shift;
-	print "Location: http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}\n\n";
+	if ($main::ENV{'HTTP_HOST'})
+	{
+		print "Location: http://$main::ENV{'HTTP_HOST'}$main::ENV{'REQUEST_URI'}\n\n";
+	}
 	main::_log("SIG '$signame' [EXIT] (timeout $TOM::fcgi_timeout secs, lives ".(time()-$TOM::time_start)." secs, $tom::count requests) PID:$$ domain:$tom::H",3,"pub.mng",1);
 	exit(0);
 }
@@ -20,15 +23,18 @@ sub handler_check
 	my $signame=shift;
 	if ($main::sig_term) # proces je v stave ze moze byt ukonceny
 	{
-		print "Location: http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}\n\n";
-		main::_log("SIG '$signame' [CHECK-EXIT] (lives ".
+		if ($main::ENV{'HTTP_HOST'})
+		{
+			print "Location: http://$main::ENV{'HTTP_HOST'}$main::ENV{'REQUEST_URI'}\n\n";
+		}
+		main::_log("SIG '$signame' [CHECK-EXIT] (sleep=$main::sig_term lives ".
 			(time()-$TOM::time_start).
 			" secs, $tom::count requests) PID:$$ domain:$tom::H ($@ $!)",3,"pub.mng",1);
 		exit(0);
 	}
 	else
 	{
-		main::_log("SIG '$signame' [CHECK-WAIT] (lives ".
+		main::_log("SIG '$signame' [CHECK-WAIT] (sleep=$main::sig_term lives ".
 			(time()-$TOM::time_start).
 			" secs, $tom::count requests) PID:$$ domain:$tom::H ($@ $!)",3,"pub.mng",1);
 		$tom::HUP=2; # po dobehnuti tohto requestu na 100% exitnem
@@ -38,7 +44,10 @@ sub handler_check
 sub handler_ignore
 {
 	my $signame = shift;
-	print "Location: http://$ENV{HTTP_HOST}$ENV{REQUEST_URI}\n\n";
+	if ($main::ENV{'HTTP_HOST'})
+	{
+		print "Location: http://$main::ENV{'HTTP_HOST'}$main::ENV{'REQUEST_URI'}\n\n";
+	}
 	main::_log("SIG '$signame' [IGNORE] (timeout $TOM::fcgi_timeout secs, lives ".(time()-$TOM::time_start)." secs, $tom::count requests) PID:$$ domain:$tom::H",3,"pub.mng",1);
 }
 
@@ -63,15 +72,52 @@ our $action_check = POSIX::SigAction->new(
 	$sigset,
 	&POSIX::SA_NODEFER);
 	
+=head1 SIGNALS
+
+=cut
 	
-main::_log("registering SIG{ALRM} action to EXIT");
-POSIX::sigaction(&POSIX::SIGALRM, $action_exit);
+# when enabled, apache2 on FreeBSD caused too much errors
+# Broken pipe
+#
+#main::_log("registering SIG{ALRM} action to EXIT");
+#POSIX::sigaction(&POSIX::SIGALRM, $action_exit);
+#POSIX::sigaction(&POSIX::SIGALRM, $TOM::Engine::pub::SIG::action_exit);
+
+=head2 HUP
+
+=cut
 
 main::_log("registering SIG{HUP} action to CHECK");
 POSIX::sigaction(&POSIX::SIGHUP, $action_check);
 
+=head2 TERM
+
+TERM is most used SIG in fastcgi processing. Apache is using this SIG to kill process when has low usage or apache restarts. Developers can use this SIG to restart processes using tom3-restart console command.
+
+Problems:
+
+=over
+
+=item *
+
+When TERM is called during request, and TERM is executed to exit(), then request is not finished and none page is given to user
+
+=item *
+
+When TERM is not executed if apache restarts, then process lost connection to apache and continue runing without deal.
+
+=back
+
+=cut
+
 main::_log("registering SIG{TERM} action to CHECK");
 POSIX::sigaction(&POSIX::SIGTERM, $action_check);
+
+main::_log("registering SIG{SIGPIPE} action to EXIT");
+POSIX::sigaction(&POSIX::SIGPIPE, $action_exit);
+
+main::_log("registering SIG{SIGUSR1} action to CHECK");
+POSIX::sigaction(&POSIX::SIGUSR1, $action_check);
 
 main::_log("start counting timeout $TOM::fcgi_timeout");
 alarm($TOM::fcgi_timeout); # zacnem pocitat X sekund kym nedostanem request
