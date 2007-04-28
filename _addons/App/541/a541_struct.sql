60727c20042dbbfaa27704056e82e90e259aca4d
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-04-28 14:16:16 +0000

    [+][frame:401,541] dummy implementation for testing XULadmin purposes

---
+++ _addons/App/541/a541_struct.sql
@@ -0,0 +1,22 @@
+-- db_h=main
+-- app=a541
+-- version=4.1
+
+-- --------------------------------------------------
+
+CREATE TABLE `/*db_name*/`.`/*app*/_file_dir` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `name_url` varchar(128) character set ascii NOT NULL default '',
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `lng` char(2) character set ascii NOT NULL default '',
+  `status` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`datetime_create`),
+  UNIQUE KEY `UNI_0` (`ID_entity`,`lng`),
+  UNIQUE KEY `UNI_1` (`ID_charindex`,`lng`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------
+
