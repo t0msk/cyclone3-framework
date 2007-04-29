31d2d572f706f6b340d198ccf35307e3906c039e
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-04-29 17:04:25 +0000

    [+][frame:a401] 401-article_cat_list - listing of categories and article items

--- _addons/App/401/a401_struct.sql
+++ _addons/App/401/a401_struct.sql
@@ -4,6 +4,21 @@
 
 -- --------------------------------------------------
 
+CREATE TABLE `/*db_name*/`.`/*app*/_article` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `ID_category` bigint(20) unsigned default NULL,
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `name_url` varchar(128) character set ascii NOT NULL default '',
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `lng` char(2) character set ascii NOT NULL default '',
+  `status` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`datetime_create`),
+  UNIQUE KEY `UNI_0` (`ID_entity`,`lng`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------
+
 CREATE TABLE `/*db_name*/`.`/*app*/_article_cat` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL,
