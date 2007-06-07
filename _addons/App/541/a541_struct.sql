f6d8817f70d2e7ec529b39de9bcd5f25da7fa794
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-06-07 19:45:09 +0000

    [*][frame:a541] a541_struct.sql - reimplemented POSIX rights

--- _addons/App/541/a541_struct.sql
+++ _addons/App/541/a541_struct.sql
@@ -1,6 +1,6 @@
 -- db_h=main
 -- app=a541
--- version=4.1
+-- version=5.0
 
 -- --------------------------------------------------
 
@@ -10,11 +10,34 @@ CREATE TABLE `/*db_name*/`.`/*app*/_file` (
   `ID_category` bigint(20) unsigned default NULL,
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
+  `name_hash` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_group` int(10) unsigned NOT NULL,
+  `posix_rights` char(9) character set ascii NOT NULL default 'rwxrw-r--',
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
   `status` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`datetime_create`),
-  UNIQUE KEY `UNI_0` (`ID_entity`,`lng`)
+  UNIQUE KEY `UNI_0` (`ID_entity`,`lng`),
+  UNIQUE KEY `UNI_1` (`name_hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------
+
+CREATE TABLE `/*db_name*/`.`/*app*/_file_j` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `ID_category` bigint(20) unsigned default NULL,
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `name_url` varchar(128) character set ascii NOT NULL default '',
+  `name_hash` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_group` int(10) unsigned NOT NULL,
+  `posix_rights` char(9) character set ascii NOT NULL default 'rwxrw-r--',
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `lng` char(2) character set ascii NOT NULL default '',
+  `status` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`datetime_create`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
@@ -25,9 +48,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_file_dir` (
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
-  `owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
-  `group` int(10) unsigned NOT NULL,
-  `rights` char(9) character set ascii NOT NULL default 'rwx------',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_group` int(10) unsigned NOT NULL,
+  `posix_rights` char(9) character set ascii NOT NULL default 'rwxrw-r--',
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
   `status` char(1) character set ascii NOT NULL default 'N',
@@ -44,9 +67,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_file_dir_j` (
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
-  `owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
-  `group` int(10) unsigned NOT NULL,
-  `rights` char(9) character set ascii NOT NULL default 'rwx------',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_group` int(10) unsigned NOT NULL,
+  `posix_rights` char(9) character set ascii NOT NULL default 'rwxrw-r--',
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
   `status` char(1) character set ascii NOT NULL default 'N',
