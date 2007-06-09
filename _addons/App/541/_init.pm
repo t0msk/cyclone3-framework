996ba8f0ea7a95d56d6f78dce7c85f4b210cf90d
rfordinal <rfordinal@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-06-09 11:53:09 +0000

    [*][frame:a541] changed files storage directory

--- _addons/App/541/_init.pm
+++ _addons/App/541/_init.pm
@@ -15,6 +15,37 @@ BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
 
 our $VERSION='$Rev$';
 
+use File::Path;
+
+BEGIN
+{
+	
+	my $htaccess=qq{
+	# safe data
+		RewriteEngine Off
+		Deny from All
+	};
+	
+	# check media directory
+	if ($tom::P)
+	{
+		
+		if (!-e $tom::P.'/!media/a541/files')
+		{
+			File::Path::mkpath $tom::P.'/!media/a541/files';
+		}
+		
+		if (!-e $tom::P.'/!media/a541/.htaccess')
+		{
+			open (HND,'>'.$tom::P.'/!media/a541/.htaccess');
+			print HND $htaccess;
+			close HND;
+		}
+		
+	}
+	
+}
+
 use App::020::_init; # data standard 0
 use App::541::functions;
 
