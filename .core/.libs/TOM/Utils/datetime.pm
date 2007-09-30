9248fe32b35c32678a432839d0418c42a8c8b544
webcom <webcom@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-09-30 13:39:19 +0000

    [!][frame:libs] TOM::Utils::datetime::mail_current() - fixed date format to respect RFC

--- .core/.libs/TOM/Utils/datetime.pm
+++ .core/.libs/TOM/Utils/datetime.pm
@@ -6,7 +6,7 @@ BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
 
 sub mail_current
 {
-	my $date=`date "+%a,%e %b %Y %H:%M:%S %z (%Z)"`;
+	my $date=`date "+%a, %e %b %Y %H:%M:%S %z (%Z)"`;
 	$date=~s|[\n\r]||g;
 	return $date;
 }
