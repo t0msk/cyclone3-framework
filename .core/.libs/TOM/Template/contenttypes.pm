fe7865591b7bea5e017949dcb9b1073ea77ca73b
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-10-10 18:46:30 +0000

    [+][frame:libs] TOM::Template::contenttypes - Template management Content-Type codes

---
+++ .core/.libs/TOM/Template/contenttypes.pm
@@ -0,0 +1,34 @@
+package TOM::Template::contenttypes;
+
+=head1 NAME
+
+TOM::Template::contenttypes
+
+=head1 DESCRIPTION
+
+Tempalte management Content-Type codes
+
+=cut
+
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+BEGIN {main::_log("<={LIB} ".__PACKAGE__)}
+
+our $debug=0;
+
+our %trans=(
+	'text/xhtml' => 'xhtml',
+	'text/html' => 'xhtml',
+	'text/xml' => 'xml'
+);
+
+sub trans
+{
+	$_[0]=$trans{$_[0]} if $trans{$_[0]};
+	return 1;
+}
+
+1;
