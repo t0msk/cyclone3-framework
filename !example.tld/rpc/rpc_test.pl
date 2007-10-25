a2aa47f1b5af0d5064b8efb95f82747deb6fdbed
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-10-25 19:25:33 +0000

    [+][frame:example.tld] rpc - added testing functionality (minor)

---
+++ !example.tld/rpc/rpc_test.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use XMLRPC::Lite;
+
+#for (1..100)
+#{
+
+my $res=XMLRPC::Lite
+      -> proxy('http://example.tld/rpc/')
+      -> call('test', {state1 => 12, state2 => 28})
+      -> result;
+
+print "output:".$res->{'lowerBound'}."\n";
+
+#}
