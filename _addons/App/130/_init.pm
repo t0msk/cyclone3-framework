8c4f1c468009d424909dde197d2db20a551cb738
webcom <webcom@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-11-04 22:10:33 +0000

    [_][frame:a130] cosmetic change (minor)

---
+++ _addons/App/130/_init.pm
@@ -0,0 +1,23 @@
+#!/bin/perl
+package App::130;
+use open ':utf8', ':std';
+use Encode;
+use encoding 'utf8';
+use utf8;
+use strict;
+
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+use App::130::processing;
+
+=head1 FUNCTIONS
+
+=cut
+
+
+=head2 GetGroups(IDhash)
+
+=cut
+
+
+1;
