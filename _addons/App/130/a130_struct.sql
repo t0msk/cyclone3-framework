560263e698f76cedf2daacd39b2fe0d201a8f08a
webcom <webcom@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-11-26 12:19:43 +0000

    [!][frame:a130] a130_struct.sql - fixed database structure to MySQL >=4.1

--- _addons/App/130/a130_struct.sql
+++ _addons/App/130/a130_struct.sql
@@ -9,9 +9,9 @@
 CREATE TABLE `/*db_name*/`.`/*app*/_received` (
   `ID` int(10) unsigned NOT NULL auto_increment,
   `rectime` int(10) unsigned NOT NULL default '0',
-  `from_name` varchar(50) NOT NULL,
+  `from_name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
   `from_email` varchar(50) character set ascii NOT NULL,
-  `to_name` varchar(50) NOT NULL,
+  `to_name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
   `to_email` varchar(50) character set ascii NOT NULL,
   `subject` varchar(255) character set ascii NOT NULL,
   `body` blob NOT NULL,
@@ -25,23 +25,24 @@ CREATE TABLE `/*db_name*/`.`/*app*/_received` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
+-- version=5.0
 
 CREATE TABLE `/*db_name*/`.`/*app*/_send` (
   `ID` int(10) unsigned NOT NULL auto_increment,
-  `ID_md5` varchar(32) character set utf8 collate utf8_bin NOT NULL default '',
+  `ID_md5` varchar(32) character set ascii collate ascii_bin NOT NULL,
   `sendtime` int(10) unsigned NOT NULL default '0',
   `priority` tinyint(4) NOT NULL default '0',
-  `from_name` varchar(20) NOT NULL default '',
-  `from_email` varchar(50) NOT NULL default '',
-  `from_host` varchar(50) NOT NULL default '',
-  `from_service` varchar(20) NOT NULL default '',
-  `to_name` varchar(50) NOT NULL default '',
-  `to_email` varchar(255) NOT NULL default '',
-  `to_cc` varchar(250) NOT NULL default '',
-  `to_bcc` varchar(250) NOT NULL default '',
-  `body` longtext NOT NULL,
-  `lng` char(2) NOT NULL default '',
-  `active` char(1) NOT NULL default 'N',
+  `from_name` varchar(20) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `from_email` varchar(50) character set ascii NOT NULL,
+  `from_host` varchar(50) character set ascii NOT NULL,
+  `from_service` varchar(20) character set ascii NOT NULL,
+  `to_name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `to_email` varchar(255) character set ascii NOT NULL,
+  `to_cc` varchar(250) character set ascii NOT NULL,
+  `to_bcc` varchar(250) character set ascii NOT NULL,
+  `body` blob NOT NULL,
+  `lng` char(2) character set ascii NOT NULL,
+  `active` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`),
   KEY `ID_md5` (`ID_md5`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
