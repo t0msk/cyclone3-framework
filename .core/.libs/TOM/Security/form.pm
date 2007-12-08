e8eb9ffc7078bd8c854878a1d8c99b39d72a66ec
webcom <webcom@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2007-12-08 23:09:52 +0000

    [+][frame:pub] tom3 - allow to CHECK_FORM from TypeID

---
+++ .core/.libs/TOM/Security/form.pm
@@ -0,0 +1,77 @@
+package TOM::Security::form;
+use strict;
+
+=head1 NAME
+
+TOM::Security::form
+
+=head1 DESCRIPTION
+
+Protect to SQL injection or HTML injection
+
+=cut
+
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+
+=head1 FUNCTIONS
+
+=head2 check_form(%)
+
+
+=cut
+
+sub check_form
+{
+	my %env=@_;
+	return 1 unless $main::FORM{$env{'variable'}};
+	main::_log("check_form variable='$env{'variable'}' type='$env{'type'}' action='$env{'action'}'");
+	
+	my $bad;
+	
+	if ($env{'type'} eq "int")
+	{
+		$bad=1 unless $main::FORM{$env{'variable'}}=~/^([0-9]+)$/;
+	}
+	elsif ($env{'type'} eq "string")
+	{
+	}
+	elsif (!$env{'type'})
+	{
+		$bad=2;
+		# do the action
+	}
+	
+	if ($bad)
+	{
+		if ($bad==1)
+		{
+			main::_log("trying to inject value '$main::FORM{$env{'variable'}}' to '$env{'variable'}'",1);
+			main::_log("trying to inject value '$main::FORM{$env{'variable'}}' to '$env{'variable'}'",4,"secure");
+			main::_log("[$tom::H] trying to inject value '$main::FORM{$env{'variable'}}' to '$env{'variable'}'",4,"secure",1);
+		}
+		
+		if ($env{'action'} eq "destroy" || !$env{'action'})
+		{
+			undef $main::FORM{$env{'variable'}};
+		}
+		elsif ($env{'action'} eq "sql_escape")
+		{
+			$main::FORM{$env{'variable'}} = $main::DB{'main'}->quote( $main::FORM{$env{'variable'}} );
+			$main::FORM{$env{'variable'}} =~s|^'||;
+			$main::FORM{$env{'variable'}} =~s|'$||;
+			#main::_log("quoted to '$main::FORM{$env{'variable'}}'");
+		}
+		else
+		{
+			
+		}
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+
+1;
