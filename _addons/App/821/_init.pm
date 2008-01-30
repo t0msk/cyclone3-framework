e67ccd77351ba6c5995a9d56051f21a1c67f1ff9
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-01-30 19:14:39 +0000

    [+][frame:a821] a821_struct.sql - journalized database structure

---
+++ _addons/App/821/_init.pm
@@ -0,0 +1,58 @@
+#!/bin/perl
+package App::821;
+use open ':utf8', ':std';
+use Encode;
+use encoding 'utf8';
+use utf8;
+use strict;
+
+
+
+=head1 NAME
+
+Application 821 - Discussions
+
+=head1 DESCRIPTION
+
+Application which manages discussions
+
+=cut
+
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+
+
+=head1 SYNOPSIS
+
+ use App::821::_init;
+
+=cut
+
+=head1 DEPENDS
+
+=over
+
+=item *
+
+L<App::301::_init|app/"301/_init.pm">
+
+=back
+
+=cut
+
+use App::301::_init;
+
+
+
+our $db_name=$App::821::db_name || $TOM::DB{'main'}{'name'};
+
+
+
+=head1 AUTHOR
+
+Comsultia, Ltd. (open@comsultia.com)
+
+=cut
+
+
+1;
--- _addons/App/821/a821_struct.sql
+++ _addons/App/821/a821_struct.sql
@@ -23,6 +23,25 @@ CREATE TABLE `/*db_name*/`.`/*app*/_discussion` (
 
 -- --------------------------------------------------------
 
+CREATE TABLE `/*db_name*/`.`/*app*/_discussion_j` (
+  `ID` mediumint(8) unsigned NOT NULL auto_increment,
+  `ID_entity` mediumint(8) unsigned default NULL,
+  `ID_category` bigint(20) unsigned default NULL, -- rel discussion_forum.ID
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `name_url` varchar(128) character set ascii NOT NULL default '',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_group` int(10) unsigned NOT NULL,
+  `posix_perms` char(9) character set ascii NOT NULL default 'rwxrw-r--',
+  `datetime_create` datetime NOT NULL,
+  `datetime_start` datetime NOT NULL,
+  `datetime_end` datetime default NULL,
+  `lng` char(2) character set ascii NOT NULL default '',
+  `status` char(1) character set ascii NOT NULL default 'Y',
+  PRIMARY KEY  (`ID`,`datetime_create`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
 CREATE TABLE `/*db_name*/`.`/*app*/_discussion_forum` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL,
@@ -42,6 +61,23 @@ CREATE TABLE `/*db_name*/`.`/*app*/_discussion_forum` (
 
 -- --------------------------------------------------------
 
+CREATE TABLE `/*db_name*/`.`/*app*/_discussion_forum_j` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `name_url` varchar(128) character set ascii NOT NULL default '',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_group` int(10) unsigned NOT NULL,
+  `posix_perms` char(9) character set ascii NOT NULL default 'rwxrw-r--',
+  `datetime_create` datetime NOT NULL,
+  `lng` char(2) character set ascii NOT NULL default '',
+  `status` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`datetime_create`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
 CREATE TABLE `/*db_name*/`.`/*app*/_discussion_message` (
   `ID` mediumint(8) unsigned NOT NULL auto_increment,
   `ID_entity` mediumint(8) unsigned default NULL,
@@ -57,6 +93,24 @@ CREATE TABLE `/*db_name*/`.`/*app*/_discussion_message` (
   `status` char(1) character set ascii NOT NULL default 'Y',
   PRIMARY KEY  (`ID`,`datetime_create`),
   UNIQUE KEY `UNI_0` (`ID_entity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+CREATE TABLE `/*db_name*/`.`/*app*/_discussion_message_j` (
+  `ID` mediumint(8) unsigned NOT NULL auto_increment,
+  `ID_entity` mediumint(8) unsigned default NULL,
+  `ID_charindex` varchar(250) character set ascii collate ascii_bin default NULL,
+  `ID_discussion` bigint(20) unsigned default NULL, -- rel discussion.ID
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `datetime_create` datetime NOT NULL,
+  `owner_anonymous_name` varchar(64) default NULL,
+  `owner_IP` varchar(16) NOT NULL,
+  `body` varchar(128) character set ascii NOT NULL default '',
+  `lng` char(2) character set ascii NOT NULL default '',
+  `status` char(1) character set ascii NOT NULL default 'Y',
+  PRIMARY KEY  (`ID`,`datetime_create`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
