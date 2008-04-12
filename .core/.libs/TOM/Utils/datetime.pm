67f5c25a754d3578ce9e711363d77fe29d4f837a
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-04-12 12:44:56 +0000

    [+][frame:libs] TOM::Utils::datetime - added function datetime_collapse() to parse date from string

--- .core/.libs/TOM/Utils/datetime.pm
+++ .core/.libs/TOM/Utils/datetime.pm
@@ -12,4 +12,38 @@ sub mail_current
 }
 
 
+sub datetime_collapse
+{
+	my $datetime_string=shift;
+	my %datetime;
+	
+	if ($datetime_string=~/^(\d+)\-(\d+)\-(\d+) (\d+):(\d+):(\d+)/)
+	{
+		$datetime{'year'}=$1;
+		$datetime{'month'}=$2;
+		$datetime{'mday'}=$3;
+		
+		$datetime{'hour'}=$4;
+		$datetime{'min'}=$5;
+		$datetime{'sec'}=$6;
+	}
+	elsif ($datetime_string=~/^(\d+)\-(\d+)\-(\d+) (\d+):(\d+)/)
+	{
+		$datetime{'year'}=$1;
+		$datetime{'month'}=$2;
+		$datetime{'mday'}=$3;
+		
+		$datetime{'hour'}=$4;
+		$datetime{'min'}=$5;
+	}
+	elsif ($datetime_string=~/^(\d+)\-(\d+)\-(\d+)/)
+	{
+		$datetime{'year'}=$1;
+		$datetime{'month'}=$2;
+		$datetime{'mday'}=$3;
+	}
+	
+	return %datetime;
+}
+
 1;
