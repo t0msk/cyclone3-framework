ed228d7f4ba8b24fa640f5c8bd02489dea96f3e5
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-04-25 06:31:56 +0000

    [+][frame:a010] added blind a010 addon

---
+++ _addons/App/010/_init.pm
@@ -0,0 +1,22 @@
+#!/bin/perl
+package App::010;
+
+=head1 NAME
+
+App::010
+
+=cut
+
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
+
+=head1 DESCRIPTION
+
+Initial library of generic local application L<010|app/"010/">.
+
+=cut
+
+1;
