9189ea78ec4892e5fb65ebdf64e7215cb1889916
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-04-25 05:42:41 +0000

    [+][frame:libs] TOM::Domain - load local.conf automatically (major)

--- .core/.libs/TOM/Domain.pm
+++ .core/.libs/TOM/Domain.pm
@@ -25,6 +25,33 @@ BEGIN
 	{
 		mkdir $tom::P.'/_logs' if (! -e $tom::P.'/_logs');
 		chmod 0777,$tom::P.'/_logs';
+		
+		# load configured applications
+		#main::_log("load addons");
+		if ($tom::P ne $TOM::P)
+		{
+			main::_log("require $tom::P/local.conf");
+			require $tom::P."/local.conf";
+			
+			main::_log("load configured addons");
+			foreach my $addon(sort keys %tom::addons)
+			{
+				my $addon_path;
+				if ($addon=~s/^a//)
+				{
+					$addon_path='App::'.$addon.'::_init';
+				}
+				elsif ($addon=~s/^e//)
+				{
+					$addon_path='Ext::'.$addon.'::_init';
+				}
+				main::_log("<={ADDON} '$addon_path'");
+				eval "use $addon_path;";
+				if ($@){main::_log("can't load addon '$addon_path' $@ $!",1)}
+				#require 
+			}
+		}
+		
 	}
 }
 
