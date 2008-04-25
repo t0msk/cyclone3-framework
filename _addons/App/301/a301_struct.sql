4ade7cc1ac6501bf2099a00c99bfd6dc6e3fa0b1
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-04-25 06:34:13 +0000

    [+][frame:a301] a301_struct.sql - added permissions,ACL and extended profile

--- _addons/App/301/a301_struct.sql
+++ _addons/App/301/a301_struct.sql
@@ -1,11 +1,11 @@
 -- db_h=main
--- db_name=TOM
--- app=a301
+-- addon=a301
 -- version=5.0
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user` (
   `ID_user` varchar(8) character set utf8 collate utf8_bin NOT NULL default '',
 --  `secure_hash` varchar(16) character set utf8 collate utf8_bin NOT NULL default '',
   `login` varchar(32) character set ascii default NULL,
@@ -13,12 +13,14 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user` (
   `autolog` char(1) character set ascii NOT NULL default 'N',
   `hostname` varchar(64) character set ascii NOT NULL,
   `datetime_register` datetime NOT NULL,
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
   `datetime_last_login` datetime default NULL,
   `requests_all` smallint(5) unsigned NOT NULL default '0',
   `email` varchar(64) character set ascii default NULL, -- internal email
   `email_verified` char(1) character set ascii NOT NULL default 'N',
   `saved_cookies` blob NOT NULL,
   `saved_session` blob NOT NULL,
+  `perm_roles_override` blob,
   `status` char(1) character set ascii NOT NULL default 'Y',
   PRIMARY KEY  (`ID_user`),
   UNIQUE KEY `UNI_0` (`hostname`,`login`),
@@ -29,8 +31,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_inactive` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_inactive` (
   `ID_user` varchar(8) character set utf8 collate utf8_bin NOT NULL default '',
 --  `secure_hash` varchar(16) character set utf8 collate utf8_bin NOT NULL default '',
   `login` varchar(32) character set ascii default NULL,
@@ -38,19 +41,22 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_inactive` (
   `autolog` char(1) character set ascii NOT NULL default 'N',
   `hostname` varchar(64) character set ascii NOT NULL,
   `datetime_register` datetime NOT NULL,
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
   `datetime_last_login` datetime default NULL,
   `requests_all` smallint(5) unsigned NOT NULL default '0',
   `email` varchar(64) character set ascii default NULL, -- internal email
   `email_verified` char(1) character set ascii NOT NULL default 'N',
   `saved_cookies` blob NOT NULL,
   `saved_session` blob NOT NULL,
+  `perm_roles_override` blob,
   `status` char(1) character set ascii NOT NULL default 'Y',
   PRIMARY KEY  (`ID_user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_emailverify` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_emailverify` (
   `ID_user` varchar(8) character set utf8 collate utf8_bin NOT NULL default '',
   `email` varchar(64) character set ascii default NULL,
   `datetime_register` datetime NOT NULL,
@@ -60,11 +66,13 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_emailverify` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_profile` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_profile` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` varchar(8) character set utf8 collate utf8_bin NOT NULL default '', -- rel _user.ID_user
   `datetime_create` datetime NOT NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin NOT NULL,
   `firstname` varchar(32) character set utf8 collate utf8_bin default NULL,
   `middlename` varchar(64) character set utf8 collate utf8_bin default NULL,
   `surname` varchar(64) character set utf8 collate utf8_bin default NULL,
@@ -72,6 +80,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile` (
   `name_suffix` varchar(16) character set utf8 collate utf8_bin default NULL,
   `gender` char(1) character set ascii default NULL,
   `date_birth` date default NULL,
+  `pin` varchar(64) character set utf8 collate utf8_bin default NULL,
   `country` varchar(64) character set utf8 collate utf8_unicode_ci default NULL,
   `country_code` char(3) character set ascii default NULL,
   `state` varchar(64) character set utf8 collate utf8_unicode_ci default NULL,
@@ -86,6 +95,8 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile` (
   `phone_home` varchar(32) character set utf8 collate utf8_unicode_ci default NULL,
   `email_public` varchar(64) character set ascii default NULL,
   `email_office` varchar(64) character set ascii default NULL,
+  `address_current` text character set utf8 collate utf8_unicode_ci,
+  `address_postal` text character set utf8 collate utf8_unicode_ci,
   `about_me` text character set utf8 collate utf8_unicode_ci,
   `lng` char(2) character set ascii NOT NULL default 'xx',
   `status` char(1) character set ascii NOT NULL default 'Y',
@@ -100,11 +111,13 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_j` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_profile_j` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` varchar(8) character set utf8 collate utf8_bin NOT NULL default '', -- rel _user.ID_user
   `datetime_create` datetime NOT NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin NOT NULL,
   `firstname` varchar(32) character set utf8 collate utf8_bin default NULL,
   `middlename` varchar(64) character set utf8 collate utf8_bin default NULL,
   `surname` varchar(64) character set utf8 collate utf8_bin default NULL,
@@ -112,6 +125,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_j` (
   `name_suffix` varchar(16) character set utf8 collate utf8_bin default NULL,
   `gender` char(1) character set ascii default NULL,
   `date_birth` date default NULL,
+  `pin` varchar(64) character set utf8 collate utf8_bin default NULL,
   `country` varchar(64) character set utf8 collate utf8_unicode_ci default NULL,
   `country_code` char(3) character set ascii default NULL,
   `state` varchar(64) character set utf8 collate utf8_unicode_ci default NULL,
@@ -126,6 +140,8 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_j` (
   `phone_home` varchar(32) character set utf8 collate utf8_unicode_ci default NULL,
   `email_public` varchar(64) character set ascii default NULL,
   `email_office` varchar(64) character set ascii default NULL,
+  `address_current` text character set utf8 collate utf8_unicode_ci,
+  `address_postal` text character set utf8 collate utf8_unicode_ci,
   `about_me` text character set utf8 collate utf8_unicode_ci,
   `lng` char(2) character set ascii NOT NULL default 'xx',
   `status` char(1) character set ascii NOT NULL default 'N',
@@ -133,20 +149,23 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_j` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_profile_view` AS (
+CREATE OR REPLACE VIEW `/*db_name*/`.`/*addon*/_user_profile_view` AS (
 	SELECT
 		
 		user.hostname,
 		user.ID_user,
+      user.posix_owner,
 		user.login,
 		user.email,
 		user.email_verified,
+      user.datetime_register,
 		user_profile.*
 		
 	FROM
-		`/*db_name*/`.`/*app*/_user` AS user
-	LEFT JOIN `/*db_name*/`.`/*app*/_user_profile` AS user_profile ON
+		`/*db_name*/`.`/*addon*/_user` AS user
+	LEFT JOIN `/*db_name*/`.`/*addon*/_user_profile` AS user_profile ON
 	(
 		user.ID_user = user_profile.ID_entity
 	)
@@ -156,8 +175,9 @@ CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_profile_view` AS (
 )
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_karma` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_profile_karma` (
   `ID_user` varchar(8) character set utf8 collate utf8_bin NOT NULL default '', -- rel _user.ID_user
   `date_event` date NOT NULL,
   `karma` double default NULL,
@@ -166,8 +186,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_karma` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_emo` ( -- experimental EMO characteristics
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_profile_emo` ( -- experimental EMO characteristics
   `ID_user` varchar(8) character set utf8 collate utf8_bin NOT NULL default '', -- rel _user.ID_user
   `date_event` date NOT NULL,
   `emo_sad` int(10) unsigned NOT NULL default '0',
@@ -180,8 +201,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_profile_emo` ( -- experimental EMO char
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_online` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_online` (
   `ID_user` varchar(8) character set utf8 collate utf8_bin NOT NULL default '',
   `ID_session` varchar(32) character set utf8 collate utf8_bin NOT NULL default '',
   `domain` varchar(64) character set ascii NOT NULL,
@@ -199,8 +221,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_online` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_online_view` AS (
+CREATE OR REPLACE VIEW `/*db_name*/`.`/*addon*/_user_online_view` AS (
 	SELECT
 		
 		user_online.ID_session,
@@ -217,8 +240,8 @@ CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_online_view` AS (
 		user.*
 		
 	FROM
-		`/*db_name*/`.`/*app*/_user_online` AS user_online
-	LEFT JOIN `/*db_name*/`.`/*app*/_user` AS user ON
+		`/*db_name*/`.`/*addon*/_user_online` AS user_online
+	LEFT JOIN `/*db_name*/`.`/*addon*/_user` AS user ON
 	(
 		user.ID_user = user_online.ID_user
 	)
@@ -226,8 +249,9 @@ CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_online_view` AS (
 )
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_group` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_group` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL,
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
@@ -235,6 +259,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_group` (
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
   `datetime_create` datetime NOT NULL,
+  `perm_roles_override` blob,
   `lng` char(2) character set ascii NOT NULL default 'xx',
   `status` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`datetime_create`),
@@ -245,8 +270,9 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_group_j` (
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_group_j` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL,
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
@@ -254,24 +280,65 @@ CREATE TABLE `/*db_name*/`.`/*app*/_user_group_j` (
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
   `datetime_create` datetime NOT NULL,
+  `perm_roles_override` blob,
   `lng` char(2) character set ascii NOT NULL default 'xx',
   `status` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`datetime_create`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
+-- db_name=TOM
 
-CREATE TABLE `/*db_name*/`.`/*app*/_user_rel_group` (
-  `ID_group` int(10) unsigned NOT NULL auto_increment, -- rel _user_group.ID
+CREATE TABLE `/*db_name*/`.`/*addon*/_user_rel_group` (
+  `ID_group` bigint(20) unsigned NOT NULL auto_increment, -- rel _user_group.ID
   `ID_user` varchar(8) character set ascii collate ascii_bin NOT NULL default '',
   PRIMARY KEY  (`ID_group`,`ID_user`),
   KEY `ID_user` (`ID_user`),
   KEY `ID_group` (`ID_group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------
+-- --------------------------------------------------------
 
-CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_rel_group_view` AS (
+CREATE TABLE `/*db_name*/`.`/*addon*/_ACL_user` ( -- table is stored where addon defined in r_prefix
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` varchar(8) character set ascii collate ascii_bin NOT NULL default '', -- rel _user.ID_user
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `functions` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `r_prefix` varchar(32) character set ascii collate ascii_bin NOT NULL,
+  `r_table` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `r_ID_entity` varchar(16) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `perm_R` char(1) character set ascii NOT NULL default 'N',
+  `perm_W` char(1) character set ascii NOT NULL default 'N',
+  `perm_X` char(1) character set ascii NOT NULL default 'N',
+  `perm_roles_override` blob,
+  `status` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`datetime_create`),
+  UNIQUE KEY `UNI_0` (`ID_entity`,`r_prefix`,`r_table`,`r_ID_entity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+CREATE TABLE `/*db_name*/`.`/*addon*/_ACL_user_group` ( -- table is stored where addon defined in r_prefix
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned NOT NULL, -- rel _user_group.ID
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `functions` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `r_prefix` varchar(32) character set ascii collate ascii_bin NOT NULL,
+  `r_table` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `r_ID_entity` varchar(16) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `perm_R` char(1) character set ascii NOT NULL default 'N',
+  `perm_W` char(1) character set ascii NOT NULL default 'N',
+  `perm_X` char(1) character set ascii NOT NULL default 'N',
+  `perm_roles_override` blob,
+  `status` char(1) character set ascii NOT NULL default 'N',
+  PRIMARY KEY  (`ID`,`datetime_create`),
+  UNIQUE KEY `UNI_0` (`ID_entity`,`r_prefix`,`r_table`,`r_ID_entity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------
+-- db_name=TOM
+
+CREATE OR REPLACE VIEW `/*db_name*/`.`/*addon*/_user_rel_group_view` AS (
 	SELECT
 		
 		user.hostname,
@@ -281,12 +348,12 @@ CREATE OR REPLACE VIEW `/*db_name*/`.`/*app*/_user_rel_group_view` AS (
 		user.login AS user_login
 		
 	FROM
-		`/*db_name*/`.`/*app*/_user_rel_group` AS rel
-	LEFT JOIN `/*db_name*/`.`/*app*/_user` AS user ON
+		`/*db_name*/`.`/*addon*/_user_rel_group` AS rel
+	LEFT JOIN `/*db_name*/`.`/*addon*/_user` AS user ON
 	(
 		user.ID_user = rel.ID_user
 	)
-	LEFT JOIN `/*db_name*/`.`/*app*/_user_group` AS user_group ON
+	LEFT JOIN `/*db_name*/`.`/*addon*/_user_group` AS user_group ON
 	(
 		user_group.ID = rel.ID_group
 	)
