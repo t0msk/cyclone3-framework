5726f93702b9d5f2510364505fd1824466634125
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-05-12 20:17:17 +0000

    [+][frame:a401] added proof-of-concept support for a301 ACL permissions

--- _addons/App/401/a301.pm
+++ _addons/App/401/a301.pm
@@ -99,6 +99,17 @@ our %groups=(
 );
 
 
+# ACL role override
+our %ACL_roles=(
+	'owner' => {
+		'article.content' => 'rwx',
+		'article.planning' => 'rwx',
+	},
+	'manager' => {
+		'article.planning' => 'r x',
+	},
+);
+
 
 # register this definition
 
@@ -106,8 +117,114 @@ App::301::perm::register(
 	'addon' => 'a401',
 	'functions' => \%functions,
 	'roles' => \%roles,
+	'ACL_roles' => \%ACL_roles,
 	'groups' => \%groups
 );
+
+
+
+sub get_owner
+{
+	shift;
+	my %env=@_;
+	my $t=track TOM::Debug(__PACKAGE__."::get_owner()");
+	
+	foreach (sort keys %env) {main::_log("input '$_'='$env{$_}'") if defined $env{$_}};
+	
+	if ($env{'r_table'} eq "article")
+	{
+		my $sql=qq{
+			SELECT
+				posix_owner
+			FROM
+				`$App::401::db_name`.a401_article_ent
+			WHERE
+				ID_entity='$env{'r_ID_entity'}'
+			LIMIT 1
+		};
+		my %sth0=TOM::Database::SQL::execute($sql,'db_h'=>'main','quiet'=>1,'-slave'=>0);
+		my %db0_line=$sth0{'sth'}->fetchhash();
+		$t->close();
+		return $db0_line{'posix_owner'};
+	}
+	elsif ($env{'r_table'} eq "article_cat")
+	{
+		my $sql=qq{
+			SELECT
+				posix_owner
+			FROM
+				`$App::401::db_name`.a401_article_cat
+			WHERE
+				ID='$env{'r_ID_entity'}'
+			LIMIT 1
+		};
+		my %sth0=TOM::Database::SQL::execute($sql,'db_h'=>'main','quiet'=>1,'-slave'=>0);
+		my %db0_line=$sth0{'sth'}->fetchhash();
+		$t->close();
+		return $db0_line{'posix_owner'};
+	}
+	
+	$t->close();
+	return undef;
+}
+
+
+sub set_owner
+{
+	shift;
+	my %env=@_;
+	my $t=track TOM::Debug(__PACKAGE__."::set_owner()");
+	
+	foreach (sort keys %env) {main::_log("input '$_'='$env{$_}'") if defined $env{$_}};
+	
+	if ($env{'r_table'} eq "article_cat")
+	{
+		App::020::SQL::functions::update(
+			'ID' => $env{'r_ID_entity'},
+			'db_h' => 'main',
+			'db_name' => $App::401::db_name,
+			'tb_name' => 'a401_article_cat',
+			'columns' =>
+			{
+				'posix_owner' => "'".$env{'posix_owner'}."'"
+			},
+			'-journalize' => 1,
+			'-posix' => 1
+		);
+		$t->close();
+		return 1;
+	}
+	elsif ($env{'r_table'} eq "article")
+	 {
+		  my @IDs=App::020::SQL::functions::get_ID_entity
+		  (
+				'ID_entity' => $env{'r_ID_entity'},
+				'db_h' => 'main',
+				'db_name' => $App::401::db_name,
+				'tb_name' => 'a401_article_ent',
+		  );
+		  if ($IDs[0]->{'ID'})
+		  {
+				App::020::SQL::functions::update(
+					 'ID' => $IDs[0]->{'ID'},
+					 'db_h' => 'main',
+					 'db_name' => $App::401::db_name,
+					 'tb_name' => 'a401_article_ent',
+					 'columns' =>
+					 {
+						  'posix_owner' => "'".$env{'posix_owner'}."'"
+					 },
+					 '-journalize' => 1,
+					 '-posix' => 1
+				);
+				$t->close();
+				return 1;
+		  }
+	 }
+	
+	$t->close();
+	return undef;
+}
 
 
 
