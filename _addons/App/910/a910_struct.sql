0ebf91f78ed07b81705dc19aa3177fe64393cef1
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2008-09-04 19:08:32 +0000

    [*][frame:a910] a910_struct.sql - updated structure of product_cat to be standardized

--- _addons/App/910/a910_struct.sql
+++ _addons/App/910/a910_struct.sql
@@ -70,26 +70,37 @@ CREATE TABLE `/*db_name*/`.`/*app*/_product_cat` (
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
+  `alias_url` varchar(128) character set ascii NOT NULL default '',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin default NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin default NULL,
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
   `status` char(1) character set ascii NOT NULL default 'N',
   PRIMARY KEY  (`ID`,`datetime_create`),
   UNIQUE KEY `UNI_0` (`ID_entity`,`lng`),
-  UNIQUE KEY `UNI_1` (`ID_charindex`,`lng`)
+  UNIQUE KEY `UNI_1` (`ID_charindex`,`lng`),
+  KEY `ID_entity` (`ID_entity`),
+  KEY `ID_charindex` (`ID_charindex`),
+  KEY `ID` (`ID`),
+  KEY `name` (`name`),
+  KEY `lng` (`lng`),
+  KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
 
 CREATE TABLE `/*db_name*/`.`/*app*/_product_cat_j` (
-  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID` bigint(20) unsigned NOT NULL,
   `ID_entity` bigint(20) unsigned default NULL,
   `ID_charindex` varchar(64) character set ascii collate ascii_bin default NULL,
   `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
   `name_url` varchar(128) character set ascii NOT NULL default '',
+  `alias_url` varchar(128) character set ascii NOT NULL default '',
+  `posix_owner` varchar(8) character set ascii collate ascii_bin default NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin default NULL,
   `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
   `lng` char(2) character set ascii NOT NULL default '',
-  `status` char(1) character set ascii NOT NULL default 'N',
-  PRIMARY KEY  (`ID`,`datetime_create`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status` char(1) character set ascii NOT NULL default 'N'
+) ENGINE=ARCHIVE DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
