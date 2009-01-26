a1f97b0954c3fc922034045254d7b5c79758eaa5
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2009-01-26 10:31:21 +0000

    [!][frame:eTatraPay] added missing _init.pm file

---
+++ _addons/Ext/TatraPay/_init.pm
@@ -0,0 +1,36 @@
+#!/bin/perl
+package Ext::TatraPay;
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+=head1 NAME
+
+Extension TatraPay
+
+=head1 DESCRIPTION
+
+Library that allow you to use TatraPay system
+
+=cut
+
+BEGIN
+{
+	main::_log("<={LIB} ".__PACKAGE__);
+	my $dir=(__FILE__=~/^(.*)\//)[0].'/src';
+	unshift @INC, $dir;
+}
+
+BEGIN {require Finance::Bank::TB;}
+
+BEGIN {shift @INC;}
+
+
+1;
+
+=head1 AUTHOR
+
+Roman Fordinal
+
+=cut
