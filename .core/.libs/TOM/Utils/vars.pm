34234f6aa269e48bed34fb02c186fd8d15de6a09
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2009-06-12 08:12:26 +0000

    [*][frame:libs] TOM::Utils::vars - disabled debug

--- .core/.libs/TOM/Utils/vars.pm
+++ .core/.libs/TOM/Utils/vars.pm
@@ -20,7 +20,7 @@ our @NCHAR=qw/0 1 2 3 4 5 6 7 8 9/;
 our @UCHAR=qw/A B C D E F G H I J K L M N O P Q R S T U V W X Y Z/;
 our @LCHAR=qw/a b c d e f g h i j k l m n o p q r s t u v w x y z/;
 
-our $debug=1;
+our $debug=0;
 
 =head1 DEPENDS
 
