738b71f1258fcbd441c77fa52b722d61f9516e21
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2009-09-25 10:19:42 +0000

    [*][frame:pub] tom3 - moved links replacement into Net::DOC - speed boost (major)

--- .core/.libs/Net/DOC.pm
+++ .core/.libs/Net/DOC.pm
@@ -112,4 +112,187 @@ sub DESTROY
 	$self={};
 }
 
+
+
+
+
+sub url_replace
+{
+	my $url_cache_enabled=1;
+	my $debug_url=0;
+	
+	
+	my $link_begin; # character before ?|?
+	my $link; # link
+	my $link_end; # character after link
+	
+	my $newlink_prefix;
+	
+	if ($tom::rewrite)
+	{
+		$link_begin=$_[0]; # ked je www_pre='/', tak to znamena ze linkujem na iny portal
+		$link=$_[1];
+		$link_end=$_[2];
+		$newlink_prefix=$tom::H_www."/" if $link_begin=~/['"]/;
+		main::_log("URL link_begin='$link_begin' link='$link' link_end='$link_end'") if $debug_url;
+	}
+	else
+	{
+		$link=$_[0];
+		$link_end=$_[1];
+	}
+	
+	main::_log("URL newlink_prefix='$newlink_prefix'") if $debug_url;
+	
+	my $cache_key="$newlink_prefix".'/'."$link";
+	if ($main::url_cache{$cache_key} && not $link=~/^\|/)
+	{
+		main::_log("URL cached for key '$cache_key'") if $debug_url;
+		main::_log("output URL '$main::url_cache{$cache_key}$link_end'") if $debug_url;
+		return "$link_begin$main::url_cache{$cache_key}$link_end";
+	}
+	
+	# neviem odkial by sa tu vzali &amp;, ale pre istotu keby to niekto tymto stylom
+	# zapisal do XSGN alebo DSGN
+	# ide o linku v zapise ?ahoj=nieco&amp;ahoj2=nieco2&amp;...
+	# ide o standardny zapis i ked my ho nepouzivame ( zatial )
+	$link=~s|&amp;|&|g;
+	
+	my %form;
+	# pridam systemovo posielane premenne __nieco
+	foreach (keys %main::FORM){$_=~/^__/ && do{$form{$_}=$main::FORM{$_};};}
+	#foreach (keys %form){main::_log("key $_")};
+	# parsing $link to %form
+	
+	if ($link=~s/^\|//)
+	{
+		main::_log("first pipe, adding QUERY_STRING_FULL") if $debug_url;
+		foreach my $cc (keys %main::pp)
+		{
+			$form{$cc}=$main::pp{$cc};
+			main::_log("set '$cc' = '$form{$cc}'") if $debug_url;
+		}
+	}
+	
+	foreach my $cc(split('&',$link))
+	{
+		my @ref=split('=',$cc);
+		if (not $cc=~/=/)
+		{
+			delete $form{$ref[0]};
+			next;
+		}
+		$form{$ref[0]}=$ref[1];
+	}
+	
+	# ochrana proti ukazaniu IAdm kluca ked dekodujem stranku v IAdm mode
+	delete $form{__key} if (($main::IAdm)&&($main::FORM{_IAdm_decode}));
+	delete $form{__key_} if (($main::IAdm)&&($main::FORM{_IAdm_decode}));
+	delete $form{__key_file} if (($main::IAdm)&&($main::FORM{_IAdm_decode}));
+	delete $form{__key} if (($main::ITst)&&($main::FORM{_ITst_decode}));
+	delete $form{__key_} if (($main::ITst)&&($main::FORM{_ITst_decode}));
+	delete $form{__key_file} if (($main::ITst)&&($main::FORM{_ITst_decode}));
+	
+	# POSLEDNE UPRAVY
+	delete $form{_dsgn} if $form{_dsgn} eq $tom::dsgn_;
+	delete $form{_lng} if $form{_lng} eq $tom::lng_;
+	# spracujem %form este cez rewrite a mozno z %form budu este vyhodne
+	# nadbytocne veci
+	if
+	(
+		($newlink_prefix ne "http://null/")&&
+		($tom::rewrite)&&
+		(my $rewrite=TOM::Net::URI::rewrite::parse_hash(\%form))
+	)
+	{
+		# REWRITE TREBA DECODOVAT!!!
+		$rewrite=TOM::Net::URI::URL::url_encode($rewrite);
+		$rewrite=~s|%2F|/|g;
+		$newlink_prefix.=$rewrite;
+	}
+	
+	# vygenerujem z %hash string
+	$link='';
+	if (keys %form > 0)
+	{
+		$link=TOM::Net::URI::URL::genGET(%form);
+	}
+	
+	# nasleduje spracovanie stringu
+	# aby som ho mal v zakodovanej podobe
+	# idem teda kodovat
+	if
+		(
+			($TOM::type_code) # kodujem
+			&&
+			($link) # v link vobec nieco je?
+			&&
+			(
+				($main::IAdm && !$main::FORM{_IAdm_decode})
+				||
+				(!$main::IAdm)
+			)
+			&&
+			(
+				($main::ITst && !$main::FORM{_ITst_decode})
+				||
+				(!$main::ITst)
+			)
+		)
+	{
+		if (
+				($main::IAdm)||($main::ITst)||($main::ENV{'HTTP_USER_AGENT'}=~/wget/i)
+			)
+		{
+			my $link_md5=Digest::MD5::md5_hex(Encode::encode_utf8($link));
+			$main::DB{sys}->Query("
+				REPLACE INTO TOM._url
+				(
+					hash,
+					url,
+					inserttime
+				)
+				VALUES
+				(
+					'$link_md5',
+					'$link',
+					'$tom::time_current'
+				)
+			");
+			main::_log("output URL '$newlink_prefix?__$link_md5$link_end'") if $debug_url;
+			$main::url_cache{$cache_key}="$newlink_prefix?__$link_md5" if $url_cache_enabled;
+			return "$link_begin$newlink_prefix?__$link_md5$link_end";
+		}
+		else  # klasicke kodovanie
+		{
+			# tomu http://null/ uz vobec nerozumiem
+			$newlink_prefix="" if ($newlink_prefix eq "http://null/");
+			my $link_hash=TOM::Net::URI::URL::hash_encode($link);
+			main::_log("output URL '$newlink_prefix?$link_hash$link_end'") if $debug_url;
+			$main::url_cache{$cache_key}="$newlink_prefix?$link_hash" if $url_cache_enabled;
+			return "$link_begin$newlink_prefix?$link_hash$link_end";
+		}
+	}
+	
+	# pokial nekodujem linku, tak oddelovace premennych '&' musia byt v linke
+	# v HTML kode ako &amp; (je to tak podla standardov)
+	$link=~s|&|&amp;|g unless $TOM::type_code;
+	$newlink_prefix="" if ($newlink_prefix eq "http://null/");
+	
+	if ($tom::rewrite && !$link) # nechcem zbytocne vypisovat ?
+	{
+		main::_log("output URL '$newlink_prefix$link_end'") if $debug_url;
+		$main::url_cache{$cache_key}="$newlink_prefix" if $url_cache_enabled;
+		return "$link_begin$newlink_prefix$link_end";
+	}
+	else
+	{
+		main::_log("output URL '$newlink_prefix?$link$link_end'") if $debug_url;
+		$main::url_cache{$cache_key}="$newlink_prefix?$link" if $url_cache_enabled;
+		return "$link_begin$newlink_prefix?$link$link_end";
+	}
+	
+}
+
+
 1;
