2ad5a06f05393fca48da2297c5231320fb484da1
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2010-04-30 14:21:09 +0000

    [+][frame:a160] database structures for a160 historization

--- _addons/App/160/a160_struct.sql
+++ _addons/App/160/a160_struct.sql
@@ -40,3 +40,40 @@ CREATE TABLE `/*db_name*/`.`/*addon*/_relation_j` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
+
+CREATE TABLE `/*db_name*/`.`/*addon*/_historization` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin NOT NULL, 
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `datetime_valid` datetime NOT NULL default '0000-00-00 00:00:00',
+  `l_prefix` varchar(32) character set ascii collate ascii_bin NOT NULL,
+  `l_table` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `l_ID_entity` varchar(16) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `l_column` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `value` varchar(256) character set utf8 collate utf8_unicode_ci default NULL, 
+  `status` char(1) character set ascii NOT NULL default 'Y',
+  PRIMARY KEY  (`ID`),
+  KEY `SEL_0` (`l_prefix`,`l_table`,`l_ID_entity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+CREATE TABLE `/*db_name*/`.`/*addon*/_historization_j` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin NOT NULL, 
+  `datetime_create` datetime NOT NULL default '0000-00-00 00:00:00',
+  `datetime_valid` datetime NOT NULL default '0000-00-00 00:00:00',
+  `l_prefix` varchar(32) character set ascii collate ascii_bin NOT NULL,
+  `l_table` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `l_ID_entity` varchar(16) character set utf8 collate utf8_unicode_ci NOT NULL,
+  `l_column` varchar(64) character set ascii collate ascii_bin NOT NULL,
+  `value` varchar(256) character set utf8 collate utf8_unicode_ci default NULL, 
+  `status` char(1) character set ascii NOT NULL default 'Y',
+  PRIMARY KEY  (`ID`,`datetime_create`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
