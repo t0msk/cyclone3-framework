44ec7af1603d2fcdda93df719bdff52895ad056e
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2011-05-03 10:31:19 +0000

    [+][fame:a710] table: a710_org new columns: latitude_decimal, longitude_decimal (geo coordinates)

--- _addons/App/710/a710_struct.sql
+++ _addons/App/710/a710_struct.sql
@@ -29,6 +29,9 @@ CREATE TABLE `/*db_name*/`.`/*addon*/_org` (
   `ZIP` varchar(16) character set ascii default NULL,
   `street` varchar(32) character set utf8 collate utf8_unicode_ci default NULL,
   `street_num` varchar(12) character set ascii default NULL,
+
+  `latitude_decimal` float(10,6) default NULL,
+  `longitude_decimal` float(10,6) default NULL,
   
   `address_postal` text character set utf8 collate utf8_unicode_ci,
   
