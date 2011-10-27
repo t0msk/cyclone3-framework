cfd00ad017921820ee907118b44b82f040ad9702
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2011-10-27 08:02:55 +0000

    [+][frame:eSolr] added Solr extension

---
+++ _addons/Ext/Solr/_init.pm
@@ -0,0 +1,59 @@
+#!/bin/perl
+package Ext::Solr;
+use open ':utf8', ':std';
+use encoding 'utf8';
+use utf8;
+use strict;
+
+=head1 NAME
+
+Extension Apache Solr
+
+=head1 DESCRIPTION
+
+Interface with the Solr (Lucene) webservice  
+
+=cut
+
+BEGIN
+{
+	main::_log("<={LIB} ".__PACKAGE__);
+}
+
+BEGIN
+{
+	require WebService::Solr;
+	$Ext::Solr=1;
+}
+
+BEGIN {shift @INC;}
+
+#our $cache_available;
+our $debug=0;
+our $service;
+
+=head1 FUNCTIONS
+
+=head2 connection()
+
+Return connection object to Solr
+
+=cut
+
+sub service
+{
+	return undef unless $Ext::Solr::url;
+	if (!$service)
+	{
+		$service = WebService::Solr->new($Ext::Solr::url, { autocommit => 0 });
+	}
+	return $service;
+}
+
+1;
+
+=head1 AUTHOR
+
+Comsultia, s.r.o. (open@comsultia.com)
+
+=cut
