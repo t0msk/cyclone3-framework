510aa4b8dbee64df2a243656e25b01acc0214537
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2012-01-18 11:08:23 +0000

    [+][frame:a542] added columns: datetime_publish_start,datetime_publish_stop to a542_file_ent to represent publish times

--- _addons/App/542/a542_struct.sql
+++ _addons/App/542/a542_struct.sql
@@ -31,6 +31,8 @@ CREATE TABLE `/*db_name*/`.`/*app*/_file_ent` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
   `ID_entity` bigint(20) unsigned default NULL, -- rel file.ID_entity
   `datetime_create` datetime NOT NULL,
+  `datetime_publish_start` datetime default NULL,
+  `datetime_publish_stop` datetime default NULL,
   `posix_owner` varchar(8) character set ascii collate ascii_bin NOT NULL,
   `posix_author` varchar(8) character set ascii collate ascii_bin default NULL,
   `posix_modified` varchar(8) character set ascii collate ascii_bin default NULL,
