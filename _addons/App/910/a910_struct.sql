9eab6aebe4a2b300c010634b9f408dea1ff79513
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2012-09-14 07:55:44 +0000

    [+][frame:a910] a new column in database: status_recommended for a910 products

--- _addons/App/910/a910_struct.sql
+++ _addons/App/910/a910_struct.sql
@@ -25,6 +25,7 @@ CREATE TABLE `/*db_name*/`.`/*app*/_product` ( -- list of modifications
   `supplier_org` bigint(20) unsigned default NULL, -- rel 710_org.ID_entity
   `supplier_person` varchar(8) character set utf8 collate utf8_bin NOT NULL default '', -- rel 301.user_ID
   `status_new` char(1) character set ascii NOT NULL default 'N',
+  `status_recommended` char(1) character set ascii NOT NULL default 'N',
   `status_sale` char(1) character set ascii NOT NULL default 'N',
   `status_special` char(1) character set ascii NOT NULL default 'N',
   `status` char(1) character set ascii NOT NULL default 'N',
