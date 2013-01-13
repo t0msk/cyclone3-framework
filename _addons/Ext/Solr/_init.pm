f5803e170e76a72bdd7e11cf33a8edc323408a91
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2013-01-13 21:08:36 +0000

    [+][frame:eSolr] added logging to all Solr commands

--- _addons/Ext/Solr/_init.pm
+++ _addons/Ext/Solr/_init.pm
@@ -45,10 +45,76 @@ sub service
 	return undef unless $Ext::Solr::url;
 	if (!$service)
 	{
-		$service = WebService::Solr->new($Ext::Solr::url, { autocommit => $Ext::Solr::autocommit });
+#		$service = WebService::Solr->new($Ext::Solr::url, { autocommit => $Ext::Solr::autocommit });
+		$service = Ext::Solr::service->new($Ext::Solr::url, { autocommit => $Ext::Solr::autocommit });
 #		$service->commit(); # first commit to store previous uncommited sessions
 	}
 	return $service;
+}
+
+package Ext::Solr::service;
+
+use parent 'WebService::Solr';
+
+sub commit
+{
+	my $self=shift;
+	
+	my ($package, $filename, $line) = caller(0);
+	
+	main::_log("[solr] COMMIT");
+	main::_log("COMMIT from $package at $filename:$line",3,"solr");
+	main::_log("[$tom::H] COMMIT from $package at $filename:$line",3,"solr",1);
+	main::_log("[$tom::H] COMMIT from $package at $filename:$line",3,"solr",2) if $tom::H ne $tom::Hm;
+	
+	$self->SUPER::commit();
+}
+
+
+sub search
+{
+	my $self=shift;
+	my ($package, $filename, $line) = caller(0);
+	
+	# call
+	my $response=$self->SUPER::search(@_);
+	
+	# process
+	if ($response)
+	{
+		my $numfound=$response->content->{'response'}->{'numFound'};
+		my $qtime=$response->content->{'responseHeader'}->{'QTime'};
+		main::_log("[solr] search '$_[0]' found='$numfound' qtime='$qtime'");
+		main::_log("search '$_[0]' found='$numfound' qtime='$qtime' from $package at $filename:$line",3,"solr");
+		main::_log("[$tom::H] search '$_[0]' found='$numfound' qtime='$qtime' from $package at $filename:$line",3,"solr",1);
+		main::_log("[$tom::H] search '$_[0]' found='$numfound' qtime='$qtime' from $package at $filename:$line",3,"solr",2) if $tom::H ne $tom::Hm;
+	}
+	else
+	{
+		main::_log("search '$_[0]'");
+	}
+	
+	return $response;
+}
+
+
+sub add
+{
+	my $self=shift;
+	my ($package, $filename, $line) = caller(0);
+	
+	my $id;
+	if ($_[0]->{'fields'}){foreach (@{$_[0]->{'fields'}}){if ($_->{'name'} eq "id"){$id=$_->{'value'};last;}}}
+	
+	# call
+	my $response=$self->SUPER::add(@_);
+	
+	main::_log("[solr] add '$id'");
+	main::_log("add '$id' from $package at $filename:$line",3,"solr");
+	main::_log("[$tom::H] add '$id' from $package at $filename:$line",3,"solr",1);
+	main::_log("[$tom::H] add '$id' from $package at $filename:$line",3,"solr",2) if $tom::H ne $tom::Hm;
+	
+	return $response;
 }
 
 1;
