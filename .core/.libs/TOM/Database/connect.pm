bc923d413e850b87287cf0995ec4c36548478d9e
comsultia <comsultia@2b51df0d-951a-0410-a6d2-bf50d5a51c0e>
2013-03-15 08:49:00 +0000

    don't inform about database connecting in sql.err.log

--- .core/.libs/TOM/Database/connect.pm
+++ .core/.libs/TOM/Database/connect.pm
@@ -131,7 +131,7 @@ sub multi
 		  
 			main::_log("DBI connecting '$handler' ('$TOM::DB{$handler}{uri}' '$TOM::DB{$handler}{user}' '****')");
 			
-			main::_log("DBI connecting '$handler' ('$TOM::DB{$handler}{uri}' '$TOM::DB{$handler}{user}' '****')",3,"sql.err");
+#			main::_log("DBI connecting '$handler' ('$TOM::DB{$handler}{uri}' '$TOM::DB{$handler}{user}' '****')",3,"sql.err");
 			
 			$main::DB{$handler} = DBI->connect
 			(
@@ -175,9 +175,9 @@ sub multi
 		{
 			$TOM::DB{$handler}{'name'}=$TOM::DB{'main'}{'name'} unless $TOM::DB{$handler}{'name'};
 			
-			main::_log("connecting '$handler' ('$TOM::DB{$handler}{host}' '$TOM::DB{$handler}{name}' '$TOM::DB{$handler}{user}' '****')");
+			main::_log("MySQL connecting '$handler' ('$TOM::DB{$handler}{host}' '$TOM::DB{$handler}{name}' '$TOM::DB{$handler}{user}' '****')");
 			
-			main::_log("connecting '$handler' ('$TOM::DB{$handler}{host}' '$TOM::DB{$handler}{name}' '$TOM::DB{$handler}{user}' '****')",3,"sql.err");
+#			main::_log("MySQL connecting '$handler' ('$TOM::DB{$handler}{host}' '$TOM::DB{$handler}{name}' '$TOM::DB{$handler}{user}' '****')",3,"sql.err");
 			
 			$main::DB{$handler} = Mysql->Connect
 			(
