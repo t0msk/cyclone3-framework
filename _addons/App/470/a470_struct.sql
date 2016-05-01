80e7ac2ced41b1fb42848bc4f00ec1c1bb63ddf1
Radomir Laucik <laucik@gmail.com>
2016-05-01 20:51:27 +0200

    added a470_event_j table to struct

--- _addons/App/470/a470_struct.sql
+++ _addons/App/470/a470_struct.sql
@@ -168,6 +168,23 @@ CREATE TABLE `/*db_name*/`.`/*addon*/_event` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------
+CREATE TABLE `/*db_name*/`.`/*addon*/_event_j` (
+  `ID` bigint(20) unsigned NOT NULL auto_increment,
+  `ID_entity` bigint(20) unsigned default NULL,
+  `ID_category` bigint(20) unsigned default NULL, -- rel _event_cat.ID_entity
+  `posix_owner` varchar(8) character set ascii collate ascii_bin default NULL,
+  `posix_modified` varchar(8) character set ascii collate ascii_bin default NULL,
+  `datetime_create` datetime NOT NULL,
+  `name` varchar(128) character set utf8 collate utf8_unicode_ci NOT NULL default '',
+  `name_url` varchar(128) character set ascii NOT NULL default '',
+  `datetime_start` datetime default NULL, -- event starts
+  `datetime_finish` datetime default NULL, -- event ends
+  `metadata` text character set utf8 collate utf8_unicode_ci NOT NULL,
+  `status` char(1) character set ascii NOT NULL default 'Y',
+  PRIMARY KEY  (`ID`,`datetime_create`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------
 
 CREATE TABLE `/*db_name*/`.`/*addon*/_event_cat` (
   `ID` bigint(20) unsigned NOT NULL auto_increment,
