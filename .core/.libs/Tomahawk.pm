33eeaaa734bad361e1a6e5e79e9b0d577d7bb52f
Comsultia <info@comsultia.com>
2016-12-29 16:10:35 +0100

    process HASH and ARRAY input parameters to cache token string

--- .core/.libs/Tomahawk.pm
+++ .core/.libs/Tomahawk.pm
@@ -370,11 +370,14 @@ sub module
 		$cache_domain=$tom::H unless $mdl_C{'-cache_master'};
 		
 		my $null;
-		foreach (sort keys %mdl_env){$_=~/^[^_]/ && do{$null.=$_."=\"".$mdl_env{$_}."\"\n";}}
+		foreach (sort keys %mdl_env){$_=~/^[^_]/ && do{
+			if (ref($mdl_env{$_}) eq "ARRAY" || ref($mdl_env{$_}) eq "HASH"){$null.=$_."=\"".$json->encode($mdl_env{$_})."\"\n";}
+			else {$null.=$_."=\"".$mdl_env{$_}."\"\n";}
+		}}
 		foreach (sort keys %mdl_C){$null.=$_."=\"".$mdl_C{$_}."\"\n";}
 		
 		$mdl_C{'-md5'}=TOM::Digest::hash($null);
-		main::_log("cache md5='".$mdl_C{'-md5'}."'") if $debug;
+		main::_log("cache md5='".$mdl_C{'-md5'}."' from string ".$null) if $debug;
 		
 		# NAZOV PRE TYP CACHE V KONFIGURAKU
 		$mdl_C{'T_CACHE'}=$mdl_C{'-addon'}."-".$mdl_C{'-name'}."-".$mdl_C{'-cache_id'};
