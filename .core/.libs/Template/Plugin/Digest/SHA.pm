d1eb602d01fb14065f643baa7585eef4f0c97ba1
Roman Fordinal <roman.fordinal@comsutlia.com>
2017-08-30 21:31:15 +0200

    add hmac_sha256_hex

--- .core/.libs/Template/Plugin/Digest/SHA.pm
+++ .core/.libs/Template/Plugin/Digest/SHA.pm
@@ -8,7 +8,7 @@ $VERSION = 0.03;
 use base qw(Template::Plugin);
 use Template::Plugin;
 use Template::Stash;
-use Digest::SHA qw(hmac_sha1_hex); 
+use Digest::SHA qw(hmac_sha1_hex hmac_sha256_hex); 
 
 #$Template::Stash::SCALAR_OPS->{'md5'} = \&_md5;
 #$Template::Stash::SCALAR_OPS->{'md5_hex'} = \&_md5_hex;
@@ -19,6 +19,7 @@ sub new {
 	
 	# now define the filter and return the plugin
 	$context->define_filter('hmac_sha1_hex', [\&_hmac_sha1_hex => 1]);
+	$context->define_filter('hmac_sha256_hex', [\&_hmac_sha256_hex => 1]);
 	
 	return bless {}, $class;
 }
@@ -29,6 +30,14 @@ sub _hmac_sha1_hex {
 	return sub {
 		my $text = shift;
 		hmac_sha1_hex($text, $args[0]);
+	}
+}
+
+sub _hmac_sha256_hex {
+	my ($context, @args) = @_;
+	return sub {
+		my $text = shift;
+		hmac_sha256_hex($text, $args[0]);
 	}
 }
 
