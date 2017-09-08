77b22beec2aaf8e8753b512054a64f05e5611ab5
Roman Fordinal <roman.fordinal@comsultia.com>
2017-09-08 14:12:56 +0200

    standardized basic brick

--- _addons/App/520/brick.pm
+++ _addons/App/520/brick.pm
@@ -9,12 +9,13 @@ use strict;
 BEGIN {eval{main::_log("<={LIB} ".__PACKAGE__);};}
 
 our $journalize=0;
-our $brick_path=$tom::P.'/!media/a520/audio';
+our $brick_path=$tom::P_media.'/a520/audio';
 
 sub audio_part_file_path
 {
 	shift;
 	my $audio=shift;
+	
 	if ($audio->{'audio_part_file.file_alt_src'})
 	{
 		$audio->{'dir'} = $audio->{'audio_part_file.file_alt_src'};
@@ -23,19 +24,84 @@ sub audio_part_file_path
 		return $audio;
 	}
 	
-	$audio->{'dir'}=$brick_path;
+	return $audio if
+		$audio->{'audio_part_file.file_alt_src'};
 	
-	my $newfile;
+	$audio->{'dir'}=$tom::P_media.'/a520/audio';
+	
+	my $create;
 	if (!$audio->{'audio_part_file.name'})
 	{
 		# generate optimized, or random 
+		my $optimal_hash;
+		if ($audio->{'audio_attrs.name'} || $audio->{'audio_part_attrs.name'})
+		{
+			$audio->{'audio.datetime_rec_start'}=~s| \d\d:\d\d$||;
+			$optimal_hash.=$audio->{'audio.datetime_rec_start'}.'-'
+				if $audio->{'audio.datetime_rec_start'};
+			$optimal_hash.=$audio->{'audio_attrs.name'}.'-'
+				if $audio->{'audio_attrs.name'};
+			$optimal_hash.=$audio->{'audio_part_attrs.name'}.'-'
+				if $audio->{'audio_part_attrs.name'};
+			$optimal_hash.=$audio->{'audio_format.ID'}.'-'
+				if $audio->{'audio_format.ID'};
+			$optimal_hash=~s|-$||;
+				
+			$optimal_hash=Int::charsets::encode::UTF8_ASCII($optimal_hash);
+			$optimal_hash=~tr/[A-Z]/[a-z]/;
+			$optimal_hash=~s|[^a-z0-9\-]|_|g;
+			1 while ($optimal_hash=~s|__|_|g);
+			my $max=120;
+			if (length($optimal_hash)>$max)
+			{
+				$optimal_hash=substr($optimal_hash,0,$max);
+			}
+		}
+		else
+		{
+			$optimal_hash=TOM::Utils::vars::genhash(8);
+		}
 		
-		my $hash=sprintf('%08d',$audio->{'audio_part.ID'})
-			.'-'.$audio->{'audio_format.ID'}.'-'.TOM::Utils::vars::genhash(4);
+		my $okay=0;
+		my $hash=$optimal_hash;
+		
+		while (!$okay)
+		{
+			my $sql=qq{
+				(
+					SELECT ID
+					FROM
+						`$App::510::db_name`.a510_audio_part_file
+					WHERE
+						name LIKE ?
+					LIMIT 1
+				)
+				UNION ALL
+				(
+					SELECT ID
+					FROM
+						`$App::510::db_name`.a510_audio_part_file_j
+					WHERE
+						name LIKE ?
+					LIMIT 1
+				)
+				LIMIT 1
+			};
+			my %sth0=TOM::Database::SQL::execute($sql,'quiet'=>1,'bind'=>[$hash,$hash]);
+			if (!$sth0{'sth'}->fetchhash())
+			{
+				main::_log("tested new audio_part_file.name '$hash'");
+				$okay=1;
+				last;
+			}
+			
+			$hash=~s|\-(....)$|-|;
+			$hash.=TOM::Utils::vars::genhash(4);
+		}
 		
 		$audio->{'audio_part_file.name'}=$hash;
 		
-		$newfile=1;
+		$create=1;
 	}
 	
 	my $id_path=sprintf('%08d',$audio->{'audio_part.ID'});
@@ -47,41 +113,38 @@ sub audio_part_file_path
 		.'/'.$audio->{'audio_part_file.name'}.'.'.$audio->{'audio_part_file.file_ext'};
 	
 	my $fullpath=$audio->{'dir'}.'/'.$audio->{'file_path'};
+	main::_log("[brick] audio_part_file_path '$fullpath' audio_format.ID=$audio->{'audio_format.ID'} audio_part_file.ID=$audio->{'audio_part_file.ID'}");
 		$fullpath=~s|^(.*)/.*?$|$1|g;
 	
-	if ($newfile && -e $audio->{'dir'} && !-d $fullpath)
+	if (!$audio->{'-notest'})
 	{
-		File::Path::mkpath($fullpath);
-		chmod 0777, $fullpath;
+		testdir(undef,$fullpath);
 	}
 	
 	return $audio;
 }
 
-
-
-sub audio_part_smil_path
+our %testdir_already;
+sub testdir
 {
 	shift;
-	my $audio=shift;
-	
-	$audio->{'dir'}=$brick_path;
-	
-	if (!$audio->{'audio_part_smil.name'})
+	my $fullpath=shift;
+	return 1 if $testdir_already{$fullpath};
+	main::_log("[brick] testdir '$fullpath'");
+	$testdir_already{$fullpath}++;
+	if (!-d $fullpath)
 	{
-		$audio->{'audio_part_smil.name'}=
-			sprintf('%08d',$audio->{'audio_part.ID'}).'-'.TOM::Utils::vars::genhash(4);
+		File::Path::mkpath($fullpath);
+		chmod 0777, $fullpath;
 	}
-	
-	my $id_path=sprintf('%08d',$audio->{'audio_part.ID'});
-	
-	$audio->{'smil_path'}=
-		substr($id_path,0,2)
-		.'/'.substr($id_path,0,4)
-		.'/'.substr($id_path,0,6)
-		.'/'.$audio->{'audio_part_smil.name'}.'.smil';
-	
-	return $audio;
+	return 1;
+}
+sub testfile
+{
+	shift;
+	my $fullpath=shift;
+	return 1 if -e $fullpath;
+	return undef;
 }
 
 1;
