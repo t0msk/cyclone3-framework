5c973ee918f5e547720d03b27a6f268e8661173c
Roman Fordinal <roman.fordinal@comsultia.com>
2017-10-10 22:03:37 +0200

    custom facedetect cascade file

--- _addons/App/501/functions.pm
+++ _addons/App/501/functions.pm
@@ -714,10 +714,12 @@ sub image_file_process
 			if ($App::501::fdetect)
 			{
 				main::_log_stdout("go fdetect");
-				my $cascade = $TOM::P.'/_addons/App/501/FaceDetect/cascade.xml';
+				my $cascade = ($App::501::fdetect_cascade_file || $TOM::P.'/_addons/App/501/FaceDetect/cascade.xml');
 				my $file = $tmpfile->{'filename'};
 				my $detector = Image::ObjectDetect->new($cascade);
 				my @faces = $detector->detect($file);
+				use Data::Dumper;
+				main::_log(Dumper(\@faces));
 				for my $face (@faces) {
 					$out.="0:".$face->{'x'}.",".$face->{'y'}."-".($face->{'x'}+$face->{'width'}).",".($face->{'y'}+$face->{'height'})."\n";
 #					main::_log_stdout("x=".$face->{'x'});
