b0ab5830016174c82259250cf2050a60bcd3e4ab
Comsultia <info@comsultia.com>
2017-12-22 08:05:05 +0100

    log METHOD

--- .core/.libs/TOM/Debug/pub.pm
+++ .core/.libs/TOM/Debug/pub.pm
@@ -76,7 +76,7 @@ sub request
 #	main::_log("testy");
 	my $severity=3;
 		$severity=4 if $env{'code'}=~/^[45]..$/;
-	main::_log("'".$main::ENV{'REQUEST_URI'}."' ".$main::ENV{'QUERY_STRING_FULL'}.' '.$main::ENV{'REMOTE_ADDR'}.' '.$env{'code'}.' '.$env{'location'},{
+		main::_log($main::ENV{'REQUEST_METHOD'}." '".$main::ENV{'REQUEST_URI'}."' ".$main::ENV{'QUERY_STRING_FULL'}.' '.$main::ENV{'REMOTE_ADDR'}.' '.$env{'code'}.' '.$env{'location'},{
 		'facility' => 'pub.track',
 		'severity' => $severity,
 		'data' => {
@@ -98,6 +98,7 @@ sub request
 			'REMOTE_ADDR_t' => $main::ENV{'REMOTE_ADDR'},
 			'REQUEST_URI_s' => $main::ENV{'REQUEST_URI'},
 			'REFERER_t' => $main::ENV{'HTTP_REFERER'},
+			'METHOD_t' => $main::ENV{'REQUEST_METHOD'},
 			'USER_AGENT_t' => $main::ENV{'HTTP_USER_AGENT'},
 			'HTTPS_s' => $main::ENV{'HTTPS'},
 #			'UserAgent_t' => $main::UserAgent_name,
